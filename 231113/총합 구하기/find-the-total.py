a8a288ef712961615f4514b83f98e06dd071de61
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-11-13 22:03:48 +0900

    Title: 총합 구하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231113/총합 구하기/find-the-total.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+
+a,b = map(int, input().split())
+
+sum = 0
+for i in range(a,b+1) :
+    if i % 6 == 0 and i % 8 != 0 :
+        sum += i
+
+print(sum)
