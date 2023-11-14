84c615433de8cc3e6f26a341a9a5df29701fb612
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-11-15 01:04:44 +0900

    Title: 총점 비교, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231115/총점 비교/compare-total-points.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+
+
+
+class Total() :
+    def __init__(self,Tuple) :
+        self.name = Tuple[0]
+        self.kor  = int(Tuple[1])
+        self.eng  = int(Tuple[2])
+        self.math = int(Tuple[3])
+        self.grade()
+    
+    def grade(self) :
+        self.grade = int(self.kor + self.eng + self.math)
+
+
+
+student_list  = []
+
+
+
+
+for _ in range(N) :
+    person = tuple(map(str, input().split()))
+    student_list.append(Total(person))
+
+student_list.sort(key = lambda x: x.grade)
+
+for person in student_list:
+    print(person.name, person.kor, person.eng, person.math)
