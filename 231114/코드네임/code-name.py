99ce404156b86c463d5ed27fae1e1ad38803a9bf
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-11-14 13:37:05 +0900

    Title: 코드네임, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231114/코드네임/code-name.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+
+class Agent :
+    def __init__(self,code_name,grade) :
+        self.n = code_name
+        self.g = grade
+
+
+agents = []
+
+for _ in range(5) :
+    code_name , grade = tuple(map(str, input().split()))
+    grade = int(grade)
+    agents.append(Agent(code_name, grade))
+
+
+mini = agents[0].g
+name = agents[0].n
+for agent in agents :
+    # print(agent.n, agent.g)
+    if agent.g < mini :
+        mini = agent.g
+        print(mini)
+        name = agent.n
+print(name, mini)
