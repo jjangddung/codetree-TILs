aa91888f96b97f48d1e3da27cadc6ad6b07c75e1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-19 00:19:56 +0900

    Title: 조건에 따른 연산, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231219/조건에 따른 연산/operatino-by-rule.py
+++ 231219/조건에 따른 연산/operatino-by-rule.py
@@ -11,11 +11,11 @@ cnt  = 0
 k = N
 
 while True :
-    if k // 2 == 0 :
+    if k % 2 == 0 :
         k = k*3 + 1
     else :
         k = k *2 + 2
     cnt +=1
     if k >= 1000 :
-        print(cnt-1)
+        print(cnt)
         break
