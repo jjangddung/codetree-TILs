9831b3ffa2d20abbd86da59fa4d493c60243eb54
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-19 00:18:09 +0900

    Title: 조건에 따른 연산, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231219/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+
+
+cnt  = 0
+
+k = N
+
+while True :
+    if k // 2 == 0 :
+        k = k*3 + 1
+    else :
+        k = k *2 + 2
+    cnt +=1
+    if k >= 1000 :
+        print(cnt-1)
+        break
