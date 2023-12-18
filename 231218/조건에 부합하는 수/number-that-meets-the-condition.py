4f8130f09b1e20e2d7dd3f5f5530cdf90a83b7f2
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-18 23:58:56 +0900

    Title: 조건에 부합하는 수, Time: 230ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231218/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,17 @@
+import sys
+
+
+input = sys.stdin.readline
+
+N = int(input())
+
+
+
+for i in range(1, N+1) :
+    if i % 2 == 0 and i % 4 != 0 :
+        continue
+    if (i // 8) % 2 == 0 :
+        continue
+    if (i % 7 ) < 4 :
+        continue
+    print(i, end= " ")
