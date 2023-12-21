31f7297ff85e05c8248bcd1646fef5ac8b5051d1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-21 22:38:19 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    double a, b ;
+    cin >> a >> b ;
+    double c ;
+    c = a +b ;
+    printf("%.2lf",c);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
