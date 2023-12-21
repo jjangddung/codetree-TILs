b92c7139fb7fd0f661b83492b23654dcd8e4038a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-21 22:44:26 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231221/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    double a,b,c ;
+
+
+    cin >> a >> b >> c;
+
+
+    cout << fixed ;
+    cout.precision(3) ;
+
+    cout <<  a;
+    cout <<  b;
+    cout << c;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
