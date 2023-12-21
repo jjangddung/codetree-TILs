aba5ce56c09d4c3ce3bd8b140d34ad683e9c23e0
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-21 23:13:52 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    int a, b ;
+    cin >> a;
+    cin.get();
+    cin >> b ;
+
+    cout << a << b;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
