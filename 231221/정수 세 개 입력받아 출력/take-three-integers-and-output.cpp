9ab1cc1158e7ba964ef741e62df4085eb436a2d2
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-21 22:46:18 +0900

    Title: 정수 세 개 입력받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+    int a ;
+    int b ;
+    int c ;
+
+
+    cin >> a >> b ;
+    cin >> c;
+
+    cout << a<<" " << b << " "<< c ;
+
+
+
+    return 0;
+}
