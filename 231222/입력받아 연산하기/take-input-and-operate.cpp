11e7e7aea8a88ba48e88271d07a2f0dc2fca92bd
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 14:56:42 +0900

    Title: 입력받아 연산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a ,b ;
+    cin >> a >> b;
+
+    cout << a + 87 ;
+    cout << endl ;
+    cout << b% 10 ;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
