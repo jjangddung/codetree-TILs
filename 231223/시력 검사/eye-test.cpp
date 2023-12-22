5500a7f8d209d83bf6fdb3f137f2325cdd351446
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-23 00:19:49 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/시력 검사/eye-test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    double a,b ;
+    cin >>a >>b;
+
+    if (a>=1.0 and b >= 1.0) {
+        cout << "High" ;
+    }
+
+    else if (a>= 0.5 and b >= 0.5) {
+        cout << "Middle" ;
+    }
+
+    else {
+        cout << "Low" ;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
