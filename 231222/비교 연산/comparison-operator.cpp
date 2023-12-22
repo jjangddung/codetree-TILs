7f71c93d0a56532990206d30bf84aa201a1ba897
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 16:48:20 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/비교 연산/comparison-operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int a,b ;
+
+    cin >> a>> b ;
+
+    cout << (a>=b ? 1:0) ;
+    cout << endl ;
+    cout << (a>b ? 1:0) ;
+    cout << endl ;
+    cout << (b>= a ? 1:0) ;
+    cout << endl ;
+    cout << (b>a ? 1:0) ;
+    cout << endl ;
+    cout << (b==a ? 1:0) ;
+    cout << endl ;
+    cout << (b==a ? 0:1) ;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
