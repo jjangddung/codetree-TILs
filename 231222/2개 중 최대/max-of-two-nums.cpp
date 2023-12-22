7948cae3a59ffb6e8da020dfc320c2a72759a096
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 15:09:52 +0900

    Title: 2개 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int a , b, result ;
+    cin >> a >>b ;
+    result = a>b ? a:b ;
+    cout <<result ;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
