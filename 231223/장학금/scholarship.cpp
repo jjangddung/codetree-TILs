b6a697f3496633afd431b029c6219ff6e2a50e6c
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-23 00:51:17 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231223/장학금/scholarship.cpp
+++ 231223/장학금/scholarship.cpp
@@ -11,7 +11,7 @@ int main() {
     if (m >= 90) {
         if(f>=90) {
             if (f>=95) {
-                cout << "100000";
+                cout << "10000";
             }
             else {
                 cout <<"50000";
