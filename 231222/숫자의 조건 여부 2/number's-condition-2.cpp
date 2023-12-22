d969541097a96ab32a6e7152ba2ae6226a4a6754
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 16:11:14 +0900

    Title: 숫자의 조건 여부 2, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int a ;
+    cin >> a;
+
+
+    if (a==5) {
+        cout << "A" ;
+
+    }
+
+    else if (a%2==0) {
+        cout << "B" ;
+    }
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
