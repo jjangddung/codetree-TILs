185532f497a7040b80b8cdfaf0b57fe6dfeb2961
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 15:51:44 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int number ;
+    cin >> number ;
+
+    if (number == 1) {
+        cout << "John" ;
+    }
+
+    else if (number==2 ){
+        cout <<"Tom";
+    }
+
+    else if  (number == 3) {
+        cout << "Paul" ;
+    }
+
+    else {
+        cout << "Vacancy" ;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
