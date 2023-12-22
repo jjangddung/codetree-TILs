a80c6fee789fce3b9c1f0811cb376ae32d2e9115
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 15:14:18 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    int grade ;
+    cin >> grade ;
+    string result ;
+    result = grade==100 ? "pass":"failure" ;
+
+    cout << result ;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
