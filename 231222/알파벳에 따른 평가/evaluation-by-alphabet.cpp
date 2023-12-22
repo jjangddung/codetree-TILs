2db53ea89d627a4becdf92104fc6ea09343d5538
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-22 16:00:26 +0900

    Title: 알파벳에 따른 평가, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    string alpha ;
+    cin >> alpha ;
+
+
+    if (alpha == "S") {
+        cout << "Superior" ;
+    }
+
+    else if(alpha == "A") {
+        cout << "Exellent" ;
+    }
+    else if(alpha == "B") {
+        cout << "Good" ;
+    }
+    else if(alpha == "C") {
+        cout << "Usually" ;
+    }
+    else if(alpha == "D") {
+        cout << "Effort" ;
+    }
+    else  {
+        cout << "Failure" ;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
