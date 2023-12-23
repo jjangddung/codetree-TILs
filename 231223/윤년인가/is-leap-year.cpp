b6155f84b23bea1587022dd92ccd554d7f28e031
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-23 12:07:58 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/윤년인가/is-leap-year.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int y ;
+
+    cin >> y  ;
+
+    if (y%4 == 0) {
+        if (y%100== 0) {
+            if(y%400 == 0) {
+                cout << "true" ;
+            }
+            else {
+                cout << "false";
+            }
+        }
+        else {
+            cout <<"true";
+        }
+    }
+    else {
+        cout <<"false";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
