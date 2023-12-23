c4326aeace3ee66fb6e98e468014958deed792ea
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-23 12:14:33 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231223/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int n ;
+    cin >> n;
+
+    if (n<=7) {
+        if (n%2 != 0) {
+            cout <<"31";
+        }
+
+        else {
+            if(n == 2) {
+                cout <<"28";
+            }
+            else  {
+                cout <<"30";
+            }
+        }
+    }
+    else {
+        if (n%2 != 0) {
+            cout <<"30";
+        }
+
+        else {
+            if(n == 2) {
+                cout <<"28";
+            }
+            else  {
+                cout <<"30";
+            }
+        }
+        
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
