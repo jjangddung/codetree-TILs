7edc48471d6eae4d20bf2d25a7a2c92266e21f4a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-23 13:37:11 +0900

    Title: 문자열 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231223/문자열 출력하기/print-string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main() {
+    for (int i = 0 ; i <6 ; i++) {
+        cout << "LeebrosCode" <<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
