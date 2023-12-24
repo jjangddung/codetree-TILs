66407b6f0a2857030b48d98d0b076363c1471958
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-25 00:10:58 +0900

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231225/369 게임/369-game.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+
+int main() {
+    int n,t,g ;
+    cin >> n;
+    string str ;
+
+
+    for (int i = 1 ; i<= n ; i ++) {
+        g = i/10 ;
+        t = i%10 ;
+        if (i%3 == 0) {
+            cout << 0 << " ";
+        }
+
+        else {
+            if ((g>=1 and g % 3== 0) or t % 3== 0) {
+                cout << 0 << " ";
+            }
+            else {
+                cout <<i << " ";
+            }
+
+
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
