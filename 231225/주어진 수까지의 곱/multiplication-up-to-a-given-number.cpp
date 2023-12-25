ef1eb18ba367af580f9b0732f0815f17ef77ed33
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-25 12:04:20 +0900

    Title: 주어진 수까지의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231225/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+    int proud = 1;
+
+    int a,b ;
+
+    cin >> a>> b;
+
+    for (int i = a ; i <=b ; i++) {
+        proud *=i ;
+    }
+
+    cout << proud ;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
