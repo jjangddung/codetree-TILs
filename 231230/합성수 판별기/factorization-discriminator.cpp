b906ba3ae7f66d419506685590c591cf5e91405c
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-30 00:59:24 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main() {
+
+    int n ;
+
+    cin >> n; 
+
+    for (int i = 2 ; i< n ; i++) {
+        if (n%i ==0) {
+            cout << "C";
+            break;
+        }
+
+        cout << "N"  ;
+
+    }
+    return 0;
+}
