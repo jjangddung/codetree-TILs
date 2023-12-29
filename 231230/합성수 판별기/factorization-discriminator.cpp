80cbb32968f57995383a5ab2459303027102ecd8
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-30 01:00:40 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/합성수 판별기/factorization-discriminator.cpp
+++ 231230/합성수 판별기/factorization-discriminator.cpp
@@ -10,15 +10,20 @@ int main() {
     int n ;
 
     cin >> n; 
+    int cnt = 0 ;
 
     for (int i = 2 ; i< n ; i++) {
         if (n%i ==0) {
             cout << "C";
+            cnt += 1;
             break;
         }
 
-        cout << "N"  ;
+        
 
+    }
+    if (cnt ==0) {
+    cout << "N"  ;
     }
     return 0;
 }
