53035a83f4a290ca89f6533a05c2a44598c1a654
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-29 23:37:41 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int n ;
+    cin >> n ;
+
+    int cnt = 0 ;
+
+
+    while (true) {
+        if (n==1) {
+            cout << cnt ;
+            break;
+        }
+        if (n % 2 == 0) {
+            cnt += 1;
+            n = n/ 2;
+        }
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
