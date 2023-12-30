7d4d836c23d278d103fa4903431d0d6c1aa69290
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-31 00:06:56 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231231/별표 출력하기 4/print-star-4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >>n ;
+
+    for (int i = n ; i >= 1 ;i --) {
+        for (int j = i ; j >= 1 ; j--) {
+            cout <<"*";
+            cout <<" ";
+        }
+        cout << endl ;
+    }
+
+    for (int i = 2 ; i <=n ;i ++) {
+        for (int j = 1 ; j <= i ; j++) {
+            cout <<"*";
+            cout <<" ";
+        }
+        cout << endl ;
+    }
+    return 0;
+}
