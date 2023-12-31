7bf0b6096a603064763408bd32e3eec6ab3c0d3e
jjangddung <123812155+jjangddung@users.noreply.github.com>
2023-12-31 23:25:45 +0900

    Title: 특정 문자의 등장 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str ;
+    cin >> str ;
+    int k = str.length();
+
+    int cnt_1 = 0 ;
+    int cnt_2 = 0 ;
+
+    for (int i = 0 ; i < k-1 ; i ++) {
+        if (str.substr(i,2)== "ee") {
+            cnt_1 +=1 ;
+
+        }
+
+        if (str.substr(i,2)== "ee") {
+            cnt_2 +=1 ;
+
+        }
+    }
+
+    cout << cnt_1 << " " << cnt_2 ;
+    return 0;
+}
