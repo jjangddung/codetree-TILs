320755257ee6ca71bb985570e701669108240d49
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-04 21:01:00 +0900

    Title: 일치하는 문자열의 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/일치하는 문자열의 수/num-of-correct-string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n ;
+    string s ;
+    cin >> n  >> s;
+
+    string str;
+    int cnt = 0 ;
+
+    for (int i = 0 ; i < 3 ; i ++) {
+        cin >> str;
+        if (str==s) {
+            cnt+=1;
+        }
+    }
+
+    cout << cnt ;
+    return 0;
+}
