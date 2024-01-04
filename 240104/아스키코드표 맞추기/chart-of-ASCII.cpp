611dd26015a64048b37a93b68d54f49a4ea5dd0d
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-04 19:46:29 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    for (int i = 0 ; i < 5 ; i++) {
+        cin >> n;
+
+        cout << (char)n << " ";
+    }
+    return 0;
+}
