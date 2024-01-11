8affff70e8ddd2e4e9ebc8422dadd9e78c2a3444
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-11 19:42:46 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n ;
+    cin >> n ;
+
+    for (int i = 1; i <= n ; i ++) {
+        if (i%2 != 0 ) {
+            cout << "*";
+        }
+        else {
+            for (int j = 1 ; j <= i ; j ++) {
+                cout << "* ";
+            }
+        }
+
+        cout << endl ;
+    }
+    return 0;
+}
