a8857a9346bf84e5fe61cc72070a0841cf5e61f6
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-12 12:14:15 +0900

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/순서쌍 만들기/making-order-pair.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = n ; i>=1; i--){
+        for (int j= n; j>=1 ; j--){
+            cout << "("<<i<<","<<j<<")";
+            cout <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
