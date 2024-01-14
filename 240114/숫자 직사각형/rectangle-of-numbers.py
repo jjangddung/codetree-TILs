93da69ff66d8cd5db712a25d391b912fbc08c384
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-14 14:41:04 +0900

    Title: 숫자 직사각형, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240114/숫자 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n,m = map(int, input().split())
+
+
+cnt = 1
+
+
+for i in range(n) :
+    for j in range(m) :
+        print(cnt, end= " ")
+        cnt+=1
+    print()
