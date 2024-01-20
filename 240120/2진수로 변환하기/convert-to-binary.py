e29e1d2a450ef768d8e6e0d32cc172b4824d0d24
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-20 13:29:33 +0900

    Title: 2진수로 변환하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240120/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n = int(input())
+
+
+arr = []
+
+while True:
+    if n < 2:
+        arr.append(n)
+        break
+
+    arr.append(n % 2)
+    n //= 2
+
+
+
+for i in arr[::-1] :
+    print(i, end ="")
