bd1fb8265a92353f774ef17ddd790fad65c71a49
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-20 14:17:32 +0900

    Title: 십진수와 이진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240120/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+sum = 0
+
+arr = input()
+
+length = int(len(arr))
+
+
+sum = 0
+
+for i in range(length) :
+    multiple = 2**(length-1-i)
+    sum += int(arr[i]) * multiple
+
+sum = sum*17
+digit_list = []
+while True :
+    if sum < 2 :
+        digit_list.append(sum)
+        break
+    digit = sum % 2
+    digit_list.append(digit)
+    sum = sum //2
+
+
+for i in digit_list[::-1] :
+    print(i, end= "")
