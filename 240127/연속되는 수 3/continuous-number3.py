bc7e8b17d3fa72444c1e53701971b6b28e24658a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-01-27 17:55:37 +0900

    Title: 연속되는 수 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240127/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,52 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n = int(input())
+
+
+
+
+arr =[]
+for _  in range(n) :
+    k = int(input())
+
+    arr.append(k)
+
+
+cnt_list = []
+
+
+for i in range(n) :
+    s = arr[i]
+    cnt = 0
+    if  s < 0 :
+        s = -1
+    else :
+        s = 1
+   
+    for j in range(i,n) :
+        if arr[j] != abs(arr[j]) :
+            if s == -1 :
+                cnt +=1
+                if j == n :
+                    cnt_list.append(cnt)
+                    
+            else :
+                cnt_list.append(cnt)
+                break
+        
+        else :
+            if s != -1 :
+                cnt +=1
+                if j  == n  :
+                    cnt_list.append(cnt)
+            
+            else :
+                cnt_list.append(cnt)
+                break
+
+
+
+print(max(cnt_list))
