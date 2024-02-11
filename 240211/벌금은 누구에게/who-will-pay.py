76e6b8842bab0894ef2a74f404fe90e2224aa243
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-11 18:13:11 +0900

    Title: 벌금은 누구에게, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240211/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,25 @@
+import sys
+
+
+input = sys.stdin.readline
+
+n,m,k = map(int, input().split())
+
+
+
+arr = [int(input()) for _ in range(m)]
+
+
+n_list = [0 for _ in range(n+1)]
+
+
+
+
+
+
+for i in arr :
+    n_list[i] +=1
+
+    if n_list[i] >= k :
+        print(i)
+        break
