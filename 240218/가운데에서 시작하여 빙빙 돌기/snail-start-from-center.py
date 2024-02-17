dde9feb168727360799b729da8f3c9f2ec768cf1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-18 01:10:24 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240218/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240218/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -8,30 +8,30 @@ n = int(input())
 matrix = [[0 for _ in  range(n)] for i in range(n)]
 
 
-dxs = [0,-1,0,1] # right up left down
-dys = [1,0,-1,0]
+dxs = [0,-1,0,1] # left up right down
+dys = [-1,0,1,0]
 
 
 dir = 0
 
 
-x,y = n//2  , n//2 
+x,y = n-1  , n-1 
 
 
-matrix[x][y] = 1
+matrix[x][y] = n*n
 
 def in_range(x,y) :
     return 0 <=x < n and 0 <= y < n
 
 
-for i in range(2,n*n +1) :
+for i in range(1,n*n) :
     nx , ny = x + dxs[dir] , y +dys[dir]
 
     if not in_range(nx,ny) or matrix[nx][ny] != 0 :
         dir = (dir +1) % 4
     
     x, y = x + dxs[dir], y +dys[dir]
-    matrix[x][y] = i
+    matrix[x][y] = n*n - i
 
 for i in range(n) :
     for j in range(n) :
