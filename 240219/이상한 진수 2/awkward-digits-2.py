4d89a614cc8267900e87e93e52e6e00faa18ca54
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-19 17:21:40 +0900

    Title: 이상한 진수 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240219/이상한 진수 2/awkward-digits-2.py
+++ 240219/이상한 진수 2/awkward-digits-2.py
@@ -14,11 +14,17 @@ for i in arr :
 # num_list = num_list[::-1]
 
 # print(num_list)
-
+count = 0
 for i in range(1, length) :
     if num_list[i] == "0" :
+        count +=1
         num_list[i] =  "1"
         break
+
+if count == 0 :
+    for i in range(length) :
+        if num_list[length-1-i] == "1" :
+            num_list[length-1-i] = "0"
 
 
 # print(num_list)
