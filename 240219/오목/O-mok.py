e9454b0496ffce0d603cd72351e4719304729c18
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-19 21:42:14 +0900

    Title: 오목, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/오목/O-mok.py
@@ -0,0 +1,61 @@
+import sys
+
+input = sys.stdin.readline
+
+
+arr = [list(map(int, input().split())) for _ in range(19)]
+
+def straight(arr, i,j) :
+    standard = arr[i][j]
+
+    if standard == 0 :
+        return False
+    
+    else :
+
+        for k in range(-2,3) :
+            if arr[i][j+k] != standard :
+                return False
+                break
+        return True
+
+
+def up_down(arr,i,j) :
+    standard = arr[i][j]
+    if standard == 0 :
+        return False
+    
+    else :
+
+        for k in range(-2,3) :
+            if arr[i+k][j] != standard :
+                return False
+                break
+
+        return True 
+
+
+def diagonal(arr,i,j) :
+    standard = arr[i][j]
+
+    if standard == 0 :
+        return False
+
+    else :
+
+        for k in range(-2,3) :
+            if arr[i+k][j+k] != standard :
+                return False
+                break
+        return True
+
+grade = 0
+
+for i in range(2,17) :
+    for j in range(2,17) :
+        if straight(arr,i,j) == True or up_down(arr,i,j) == True or diagonal(arr,i,j) == True :
+            print(arr[i][j])
+            print(i+1 , j+1)
+
+            grade +=1
+            break
