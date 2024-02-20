e6b63a081b5f67e050167a7f3ed7e65265a5c933
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-20 20:13:23 +0900

    Title: 특정 수와 근접한 합, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240220/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240220/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -6,20 +6,18 @@ n,s = map(int, input().split())
 
 arr = list(map(int, input().split()))
 
-maximum = 10000000
-
-standard = abs(sum(arr) - arr[0] - arr[1] -s)
+total =sum(arr) 
+standard = abs(total - arr[0] - arr[1] -s)
 
 for i in range(n-1) :
     for j in range(i+1,n) :
-        result = sum(arr)
 
         first = arr[i]
         second = arr[j]
         # standard = abs(first-second)
-        new = result - first - second
+        new = total - first - second
         if abs(new-s) < standard :
-            final = new-s
+            final = abs(new-s)
             standard = final
         
     
