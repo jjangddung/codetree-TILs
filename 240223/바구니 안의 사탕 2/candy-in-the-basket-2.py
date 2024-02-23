73b6855a16f151bcb112095e4ebe839669045fb5
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-23 11:09:22 +0900

    Title: 바구니 안의 사탕 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240223/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240223/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -5,16 +5,16 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-arr = [0] * 1000001
+arr = [0] * 101
 
 
 for i in range(n) :
     num , index = map(int, input().split())
-    arr[index] = num
+    arr[index] += num
 
 maximum = 0
-
 length = len(arr)
+
 for i in range(k,length-k) :
     # print(i-k,i+k-1)
     new_arr = arr[i-k:i+k+1]
