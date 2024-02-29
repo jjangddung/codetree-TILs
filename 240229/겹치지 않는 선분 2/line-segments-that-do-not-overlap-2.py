228f8f9c2c8e5f8ccc8d62cd73beca90f9d4f3e8
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-29 12:56:31 +0900

    Title: 겹치지 않는 선분 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240229/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240229/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -9,7 +9,7 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 arr.sort()
 
-# print(arr)
+# print(arr-)
 
 
 
@@ -20,6 +20,8 @@ def check_cross(num) :
     # 선 범위안에 처음부터 아예 안들어가거나, 스타트 지점이 들어간 경우 엔드 지점은 밖에 들어가야함 선 범위에
     ori_start =arr[num][0]
     ori_end = arr[num][1]
+    ori = ori_end - ori_start
+
 
     for p in range(n) :
         if p == num :
@@ -27,22 +29,21 @@ def check_cross(num) :
 
         start,end = arr[p][0] , arr[p][1]
 
-        if start > ori_start and end > ori_end :
-            pass
-        elif end <= ori_start :
-            pass
-        elif start >= ori_end :
-            pass
-        else :
+        new = end - start
+
+        up = ori_start*new - start*ori
+        down = new - ori
+        if down == 0 :
+            continue
+            
+        result = up/down
+
+        if ori_start<= result <= ori_end :
             return False
             break
     return True
 
 
-        
-
-
-        
 
 
 
