ae7a86c0b250bf506f64465ae1bd06702edb8dea
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-02-29 14:17:46 +0900

    Title: 스승의 은혜 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240229/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,26 @@
+import sys
+
+
+
+input = sys.stdin.readline
+
+n, b = map(int, input().split())
+
+
+arr = [int(input()) for _ in range(n)]
+arr.sort()
+
+count = 0
+
+for i in range(n) :
+    if b > arr[i] :
+        b -= arr[i]
+        count +=1
+    
+    else :
+        if b > arr[i]//2 :
+            b -= arr[i]//2
+            count +=1
+            break
+
+print(count)
