09fa8a073cd3c0abaddc0611b3feb75f3d6f7ea1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-01 23:46:45 +0900

    Title: 개발자의 순위, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/개발자의 순위/developer's-rank.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+k,n = map(int, input().split())
+
+
+arr = [list(map(int, input().split())) for _ in range(k)]
+
+result = 0
+
+for i in range(1,n+1) :
+
+    for j in range(1,n+1) :
+        if i == j :
+            continue
+        count = 0
+        for ele in arr :
+            if  ele.index(j) < ele.index(i) :
+                count +=1
+        if count == 3:
+            result +=1
+
+
+print(result)
