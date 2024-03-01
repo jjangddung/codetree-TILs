ade8048740c1bb60a4b4b609639bd72625c0733d
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-02 02:15:52 +0900

    Title: 스승의 은혜 3, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

--- 240302/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240302/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -9,20 +9,28 @@ n,b = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
 
-matrix.sort()
+# matrix.sort()
 
 maximum = 0
 
 for i in range(n) :
     sum_list = []
-    matrix_copy = matrix.copy()
+    matrix_copy = []
+    for w in range(n) :
+        matrix_copy.append(matrix[w])
+
+    # / print(sum_list)
+    # print(matrix_copy)
 
     for j in range(n) :
         
         if i == j :
-            matrix_copy[j][0] = matrix_copy[j][0]//2
-        ele = sum(matrix_copy[j])
+            ele = matrix_copy[j][0]//2 + matrix_copy[j][1]
+        else :
+            ele = matrix_copy[j][0] + matrix_copy[j][1]
+        # print(f'j:{j}')
         sum_list.append(ele)
+    # print(matrix_copy)
     sum_list.sort()
     result = 0
     # print(sum_list)
@@ -30,11 +38,14 @@ for i in range(n) :
     # print(b_copy)
     assume = 0
 
-    for element in sum_list :
-        assume += element
+    for p in range(n) :
+        assume += sum_list[p]
         if assume <= b :
             result +=1
-            
+        
+        else :
+            break
+
         
     maximum = max(maximum,result)
 
