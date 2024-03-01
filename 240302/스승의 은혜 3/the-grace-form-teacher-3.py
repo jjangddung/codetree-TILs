4c41347e7ed0fe7ac41120916dd4817297b70427
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-02 01:00:57 +0900

    Title: 스승의 은혜 3, Time: 287ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240302/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,38 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n,b = map(int, input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+
+matrix.sort()
+
+result_list = []
+
+for i in range(n) :
+    sum_list = []
+
+    for j in range(n) :
+        
+        if i == j :
+            matrix[j][0] = matrix[j][0]//2
+        ele = sum(matrix[j])
+        sum_list.append(ele)
+    sum_list.sort()
+    result = 0
+    # print(sum_list)
+
+    b_copy =b
+    for element in sum_list :
+        if b_copy >= element :
+            b_copy -= element
+            result +=1
+        else :
+            break
+    result_list.append(result)
+
+print(max(result_list))
