2c31d2c6c6003bd564c3a8df562310809f0a8b23
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-02 01:45:17 +0900

    Title: 스승의 은혜 3, Time: 302ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240302/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240302/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -15,19 +15,20 @@ result_list = []
 
 for i in range(n) :
     sum_list = []
+    matrix_copy = matrix.copy()
 
     for j in range(n) :
         
         if i == j :
-            matrix[j][0] = matrix[j][0]//2
-        ele = sum(matrix[j])
+            matrix_copy[j][0] = matrix_copy[j][0]//2
+        ele = sum(matrix_copy[j])
         sum_list.append(ele)
     sum_list.sort()
     result = 0
     # print(sum_list)
 
-    b_copy = b -1
-    b_copy +=1
+    b_copy = b
+    # print(b_copy)
     for element in sum_list :
         if b_copy >= element :
             b_copy -= element
