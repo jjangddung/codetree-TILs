d186706820d81776ac5444974a193d9ccbe15172
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-02 13:56:42 +0900

    Title: 흥미로운 숫자 2, Time: 237ms, Memory: 25MB, Status: Passed - CodeTree

--- 240302/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240302/흥미로운 숫자 2/interesting-numbers-2.py
@@ -13,8 +13,15 @@ for ele in range(start,end+1) :
         new_list.append(int(ele[length]))
     
     # print(new_list)
-    new_list = set(new_list)
-    if len(new_list) == 2 :
-        count +=1
+    set_new_list = set(new_list)
+    set_new_list = list(set_new_list)
+
+    # print(set_new_list)
+
+    if len(set_new_list) == 2:
+        if new_list.count(set_new_list[0]) ==1 or new_list.count(set_new_list[1]) == 1:
+            count +=1
+
+
 
 print(count)
