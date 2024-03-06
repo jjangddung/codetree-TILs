0c67d174bf60c93086c761e1bbc8769fbf9079b5
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-06 10:33:59 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240306/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240306/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -10,8 +10,10 @@ for i in range(1,n) :
     new_list = []
     for j in range(n) :
         new_ele = arr[j:j+i]
-        new_list.append(new_ele)
+        if j+i <= n :
+            new_list.append(new_ele)
     set_new_list = set(new_list)
+    # print(new_list)
     if len(set_new_list) == len(new_list) :
         print(i)
         break
