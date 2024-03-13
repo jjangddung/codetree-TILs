30cbe3390e4570a7b0305be4075bc6efc9805ed8
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-13 16:34:39 +0900

    Title: 구간 잘 나누기, Time: 216ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/구간 잘 나누기/divide-sections-well.py
+++ 240313/구간 잘 나누기/divide-sections-well.py
@@ -1,46 +1,45 @@
-import sys
+MAX_A = 10000
 
+#변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-input = sys.stdin.readline
+a = list(map(int, input().split()))
 
+# 100개의 숫자를 여러 개의 구간으로 나누는 경우의 수는 굉장히 많습니다.
+# 숫자들을 모든 구간으로 나누어 완전탐색하기 위해서는
+# 최악의 경우 99C50 = 약 5 * 10^28개의 경우를 세야 합니다.
+# 이는 아무리 컴퓨터라 해도 너무 많은 경우의 수를 세야 합니다.
+# 따라서 보다 효율적인 방법을 찾아야 합니다.
 
+# 잘 생각해 보면, 구간의 합의 최댓값을 결정한다면
+# 구간을 몇 개로 나눠야 하는지 손쉽게 찾을 수 있습니다.
+ans = MAX_A
+for i in range(1, MAX_A + 1):
+    # 구간의 합의 최댓값이 i일 때
 
+    # possible : 구간을 나눌 수 있으면 true
+    # section : 나뉘어져야 하는 구간의 개수
+    possible = True
+    section = 1
 
+    cnt = 0
+    for j in range(n):
+        # 숫자 하나가 i보다 크면 구간을 나눌 수 없습니다.
+        if a[j] > i:
+            possible = False
+            break
+        
+        # j번째 숫자가 들어갔을 때 i보다 커지면
+        # j번째 숫자부터 다음 구간으로 만듭니다.
+        if cnt + a[j] > i:
+            cnt = 0
+            section += 1
 
-n,m = map(int , input().split())
-arr = list(map(int , input().split()))
+        # 이번 구간에 j번째 숫자를 집어넣습니다.
+        cnt += a[j]
 
+    if possible and section == m:
+        ans = min(ans, i)
+        break
 
-def check(arr, n, m, mid):
-    count = 1
-    total = 0
-    
-    for num in arr:
-        total += num
-        if total > mid: #mid값을 기준으로 판별
-            count += 1
-            total = num
-        if count > m:
-            return False
-    
-    return True
-
-def min_max_sum(arr, n, m):
-    low = max(arr)  # 가능한 최소값
-    high = sum(arr)  # 가능한 최대값
-    result = 0
-    
-    while low <= high:
-        mid = (low + high) // 2
-        if check(arr, n, m, mid):
-            result = mid
-            high = mid - 1
-        else:
-            low = mid + 1
-    
-    return result
-
-# 예시 입력
-
-
-print(min_max_sum(arr, n, m))  # 출력: 5
+print(ans)
