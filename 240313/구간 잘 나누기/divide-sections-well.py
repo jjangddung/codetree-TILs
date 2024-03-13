5632e81ca3eda28eaf4a0d43f37b7a09d33df276
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-13 16:23:39 +0900

    Title: 구간 잘 나누기, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240313/구간 잘 나누기/divide-sections-well.py
+++ 240313/구간 잘 나누기/divide-sections-well.py
@@ -1,81 +1,46 @@
 import sys
 
+
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
 
 
-arr = list(map(int, input().split()))
-
-# list 중간에 0을 추가하는 방법?
-#0을 만나거나 끝 부분이면 종료
-# list 길이는 n+m-1
-# combination 구현 >> n+m-1Cn
-
-real = [0]*(n+m-1)
-
-# print(real)
-answer = []
-answer_list = []
 
 
-global mini
-mini = 10000000
+n,m = map(int , input().split())
+arr = list(map(int , input().split()))
 
 
-def checking(p) :
-    if len(answer) == 0 :
-        return True
-
-    else :
-        if p - max(answer) <=1 :
-           return False
-        else :
-            return True
-
-def store(num) :
-    global mini
-    if num == m-1 :
-        # answer_list.append(answer)
-        # print(answer)
-        num_list = []
-        for p in range(n+m-1) :
-            if not p in answer :
-                num_list.append(p)
-        real = [0]*(n+m-1)
-        for t in range(n) :
-            real[num_list[t]] = arr[t]
-        
-        maxi = 0
-        total = 0
-
-        for number in range(n+m-1) :
-            if real[number] == 0 or number == n+m-2 :
-                total += real[number]
-                maxi = max(total,maxi)
-                if maxi > mini :
-                    break
-                total = 0
-            
-            else :
-                total += real[number]
-        
-        mini = min(mini,maxi)
-
-        
-
-        return
-
-    for i in range(1,n+m-2) :
-        if checking(i) == True :
-            answer.append(i)
-            store(num+1)
-            answer.pop()
+def check(arr, n, m, mid):
+    count = 1
+    total = 0
     
-    return
+    for num in arr:
+        total += num
+        if total > mid: #mid값을 기준으로 판별
+            count += 1
+            total = num
+        if count > m:
+            return False
+    
+    return True
+
+def min_max_sum(arr, n, m):
+    low = max(arr)  # 가능한 최소값
+    high = sum(arr)  # 가능한 최대값
+    result = 0
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if check(arr, n, m, mid):
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return result
+
+# 예시 입력
 
 
-store(0)
-# print(answer_list)
-
-print(mini)
+print(min_max_sum(arr, n, m))  # 출력: 5
