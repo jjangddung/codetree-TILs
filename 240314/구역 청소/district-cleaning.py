3d97afc322eef73029005476b3a53926170a93ed
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-14 23:08:06 +0900

    Title: 구역 청소, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240314/구역 청소/district-cleaning.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+a,b = map(int , input().split())
+c,d = map(int , input().split())
+
+
+
+arr = [0]*101
+
+for i in range(a,b) :
+    arr[i] = 1
+
+for j in range(c,d) :
+    arr[j] = 1
+
+
+print(sum(arr))
