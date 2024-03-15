6d63870c504ec26ddc0befc9b9b7f57873a20548
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-15 23:11:34 +0900

    Title: 두 직사각형, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240315/두 직사각형/two-rectangles.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+
+fisrt_square = list(map(int, input().split()))
+second_square = list(map(int, input().split()))
+
+
+matrix = [[0 for _ in range(101)] for i in range(101)]
+
+
+
+for x in range(fisrt_square[0],fisrt_square[2]) :
+    for y in range(fisrt_square[1],fisrt_square[3]):
+        matrix[x][y] += 1
+
+count = 0
+for x in range(fisrt_square[0],fisrt_square[2]) :
+    
+    for y in range(fisrt_square[1],fisrt_square[3]):
+        if matrix[x][y] == 1 :
+            print("overlapping") 
+            count +=1
+            break
+        else :
+            matrix[x][y] += 1
+    if count != 0 :
+        break
+
+if count == 0 :
+    print('nonoverlapping')
