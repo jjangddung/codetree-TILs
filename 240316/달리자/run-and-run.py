4c759913a9b097f9bc92135a5807effef99dee48
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-16 13:21:58 +0900

    Title: 달리자, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/달리자/run-and-run.py
+++ 240316/달리자/run-and-run.py
@@ -9,7 +9,7 @@ first_value = list(map(int , input().split()))
 second_value = list(map(int, input().split()))
 
 count = 0
-
+result = 0
 for i in range(n-1) :
     if first_value[i] != second_value[i] :
         result = first_value[i] - second_value[i]
