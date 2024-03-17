474a628982ce9af9d269ca1fa4bc67d657e4e978
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-17 23:30:58 +0900

    Title: A, B, C 찾기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240317/A, B, C 찾기/finding-a-b-c.py
+++ 240317/A, B, C 찾기/finding-a-b-c.py
@@ -6,11 +6,12 @@ sum_list = list(map(int, input().split()))
 sum_list.sort()
 max_sum = max(sum_list)
 min_value = min(sum_list) 
-for a in range(min_value,max_sum) :
-    for b in range(a,max_sum) :
-        for c in range(b,max_sum) :
-            new_list = [a,b,c,a+b,a+c,b+c,a+b+c]
+for a in range(7) :
+    for b in range(a+1,7) :
+        for c in range(b+1,7) :
+            A,B,C = sum_list[a],sum_list[b],sum_list[c]
+            new_list = [A,B,C,A+B,A+C,B+C,A+B+C]
             new_list.sort()
             if new_list == sum_list :
-                print(a,b,c)
+                print(A,B,C)
                 break
