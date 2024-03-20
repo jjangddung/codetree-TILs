00828c10f6a7596059f2ae25dc8c837b8cc8e214
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-20 09:48:17 +0900

    Title: X 달리기, Time: 5092ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240320/X 달리기/x-run.py
+++ 240320/X 달리기/x-run.py
@@ -5,12 +5,12 @@ input = sys.stdin.readline
 
 distance = int(input())
 
-# Dn = Dn-1 +(+-1 or 0)
+
 
 
 
 for time in range(1,1000000) :
-    if time % 2 == 0 :
+    if time % 2 == 0 : # 최대 속도를 정의하기
         V_max = time//2
     else :
         V_max = time //2 +1
@@ -18,18 +18,17 @@ for time in range(1,1000000) :
     V_list = []
     result = 0
 
-    for v in range(1,V_max) :
-        V_list.append(v)
+    for v in range(1,V_max) : #최대 속도 전까지는 최소 그 전속도는 2번씩은 나옴
         result += 2*v
     V_list.append(V_max)
 
     
-    minus = distance- result
+    minus = distance- result #최대속도에 도달하지 않고, 다른 속도를 한 번 더 써서 도달한다면..
     if minus in V_list  :
         print(time)
         break
     else :
-        result += V_max
+        result += V_max # 최대속도 한 번 찍었다면..
         V_list.append(V_max)
         # print(V_list)
         minus -= V_max
