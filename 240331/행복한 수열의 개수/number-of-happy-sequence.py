8ea8b1e12cbd80a2c45c5297f8d6385579bf2e89
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-03-31 00:17:38 +0900

    Title: 행복한 수열의 개수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240331/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+new_line_list = []
+result = 0
+for i in range(n) :
+    new_line_1 = []
+    new_line_2 =  matrix[i]
+
+    for j in range(n) :
+        new_line_1.append(matrix[j][i])
+    
+    new_line_list.append(new_line_1)
+    new_line_list.append(new_line_2)
+
+# print(new_line_list)
+
+for line in new_line_list :
+    count = 0
+    maxi = 0
+    for p in range(n-1) :
+        if line[p] == line[p+1] :
+            if count  == 0 :
+                count +=2
+            else :
+                count +=1
+            # print("line과 count : ", line, count)
+
+            if p == n-2 :
+                maxi = max(count,maxi)
+                
+        else :
+            maxi = max(count,maxi)
+            count = 0
+    # print(maxi)
+    # print(line, maxi)
+    if maxi >= m :
+        result +=1
+
+print(result)
