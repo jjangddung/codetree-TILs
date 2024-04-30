c7d117ae8224c5f511d4d731b3e246ee06d56c27
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-04-30 12:12:16 +0900

    Title: ABC 줄 세우기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240430/ABC 줄 세우기/abc-line-up.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n = int(input())
+origin = 'A'
+answer_list = []
+
+for i in range(n) :
+    answer_list.append(chr(ord('A')+ i))
+    
+alpha_list = list(map(str, input().split()))
+
+index_list = []
+for alpha in alpha_list :
+    index =answer_list.index(alpha)
+    index_list.append(index)
+
+p  = n -1
+count = 0
+while p >= 0 :
+
+    position =index_list.index(p)
+    while position != p :
+        index_list[position] , index_list[position+1] = index_list[position+1] , index_list[position]
+        position =index_list.index(p)
+        count +=1
+    p -= 1
+
+print(count)
