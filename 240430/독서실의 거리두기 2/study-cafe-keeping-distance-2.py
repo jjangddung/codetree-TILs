8112e3eb4d9523a469958298d2b709d59eb9699a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-04-30 11:52:46 +0900

    Title: 독서실의 거리두기 2, Time: 195ms, Memory: 26MB, Status: Passed - CodeTree

--- 240430/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240430/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -23,16 +23,18 @@ for  i in range(n) :
     
     num_list[i] = 1
 
-    for index in range(n) :
-        new_table += str(num_list[index]) 
-    
-    new_string = new_table.split('1')
-    arr = []
-    for p in range(len(new_string)) :
-        if len(new_string[p]) > 0 :
-            arr.append(len(new_string[p]))
-    value = min(arr)
-    if value > min_value :
-        min_value = value
+    one_index_list = []
+
+
+    for index , num in enumerate(num_list) :
+        if num == 1 :
+            one_index_list.append(index)
+    mini = 10000
+    for s in range(1,len(one_index_list)) :
+        value = one_index_list[s] - one_index_list[s-1]
+        mini = min(value, mini)
+    min_value = max(mini, min_value)
+        
+
 
 print(min_value)
