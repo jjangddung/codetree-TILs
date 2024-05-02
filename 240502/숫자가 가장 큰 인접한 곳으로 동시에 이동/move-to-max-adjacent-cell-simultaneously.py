9ced3f39987c009985ca94dbee3628a998c2e20f
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-02 23:05:13 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -25,18 +25,23 @@ for xy in rc_list :
 
 for _ in range(t) :
     new_count_matrix = [[0]*n  for _  in range(n)]
+    new_list = []
 
 
     for xy in rc_list :
-        curr_x , curr_y = xy[0] ,xy[1]
+        curr_x , curr_y = xy[0]-1 ,xy[1]-1
+        maxi = matrix[curr_x][curr_y]
+        maxi_x , maxi_y = curr_x ,curr_y
 
         for dx,dy in zip (dxs,dys) :
             next_x , next_y = curr_x +dx , curr_y +dy
-            if in_range(next_x,next_y) and matrix[next_x][next_y] > matrix[curr_x][curr_y] :
-                curr_x, curr_y = next_x ,next_y
-                new_count_matrix[curr_x][curr_y] +=1
-                break
-    
+            if in_range(next_x,next_y) and matrix[next_x][next_y] > maxi:
+                maxi = matrix[next_x][next_y]
+                maxi_x = next_x
+                maxi_y = next_y
+        
+        new_count_matrix[maxi_x][maxi_y] = 1
+    # print(new_count_matrix)
     for r in range(n) :
         for c in range(n) :
             if new_count_matrix[r][c] > 1 :
