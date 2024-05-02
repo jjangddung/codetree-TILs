c6389e818b90374bf9d7c3526b092b7cb643485d
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-02 19:37:42 +0900

    Title: 최고의 33위치, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/최고의 33위치/best-place-of-33.py
+++ 240502/최고의 33위치/best-place-of-33.py
@@ -1,28 +1,34 @@
-import sys
-
-input = sys.stdin.readline
-
-
+# 변수 선언 및 입력
 n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
 
-matrix = [list(map(int, input().split())) for _ in range(n)]
+# (row_s, col_s) ~ (row_e, col_e) 사이의 금의 개수를 계산합니다.
+def get_num_of_gold(row_s, col_s, row_e, col_e):
+    num_of_gold = 0
+
+    for row in range(row_s, row_e + 1):
+        for col in range(col_s, col_e + 1):
+            num_of_gold += grid[row][col]
+
+    return num_of_gold
 
 
+max_gold = 0
 
-maxi = 0
-
-for i in range(n) :
-    for j in range(n) :
-        if i > n-3 or j > n-3 :
+# (row, col)이 3 * 3 격자의 좌측 상단 모서리인 경우를 전부 탐색합니다. 
+for row in range(n):
+    for col in range(n):
+        # 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
+        if row + 2 >= n or col + 2 >= n:
             continue
         
-        new_mat = matrix[i:i+3][j:j+3]
-        # print(new_mat)
-        total_sum = 0
-        for col in new_mat :
-            total_sum += sum(col)
-        maxi = max(total_sum , maxi)
+        num_of_gold = get_num_of_gold(row, col, row + 2, col + 2)
+            
+        # 최대 금의 개수를 저장합니다.
+        max_gold = max(max_gold, num_of_gold)
 
-
-print(maxi)
+print(max_gold)
