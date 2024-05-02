ffd1095d241a54b9a14b4e384ecd8a38696787a3
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-02 19:29:12 +0900

    Title: 최고의 33위치, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240502/최고의 33위치/best-place-of-33.py
+++ 240502/최고의 33위치/best-place-of-33.py
@@ -14,10 +14,11 @@ maxi = 0
 
 for i in range(n) :
     for j in range(n) :
-        if i > n-2 or j > n-2 :
+        if i > n-3 or j > n-3 :
             continue
         
-        new_mat = matrix[i:i+n][j:j+n]
+        new_mat = matrix[i:i+3][j:j+3]
+        # print(new_mat)
         total_sum = 0
         for col in new_mat :
             total_sum += sum(col)
