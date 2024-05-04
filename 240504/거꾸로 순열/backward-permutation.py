a1c7588530ae38b29ac3c4aee7f2712c33151a8a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-04 21:36:21 +0900

    Title: 거꾸로 순열, Time: 241ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240504/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,31 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n = int(input())
+answer = []
+
+
+visited_list = [False] *(n+1)
+
+def choose(curr_num) :
+    
+    if curr_num == n +1 :
+        print(*answer)
+        return
+
+    for i in range(n,0,-1) :
+        if  visited_list[i] :
+            continue
+        
+        visited_list[i] = True
+        answer.append(i)
+
+        choose(curr_num+1)
+        answer.pop()
+        visited_list[i] = False
+
+
+choose(1)
