432e013c529061954c850df8cbe2788879e06bef
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-04 23:36:06 +0900

    Title: 수들의 합 최대화하기, Time: 5024ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 240504/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,52 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n = int(input())
+answer = []
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+# print(matrix)
+visited_list = [[False]*n for _ in range(n)]
+visited_x = [False]*n
+visited_y = [False]*n
+# print(visited_list)
+        
+
+maxi = 0 
+
+def choose(num) :
+    global maxi
+    result = 0
+    
+    if num == n +1 :
+        for value in answer :
+            result += matrix[value[0]][value[1]]
+        
+        maxi = max(result,maxi)
+            
+
+        return
+
+
+    for i in range(n) :
+        for j in range(n) :
+            if visited_x[i] == True or visited_y[j] == True :
+                continue
+
+            answer.append([i,j])
+
+            visited_x[i] = True
+            visited_y[j] = True
+
+            choose(num +1)
+
+            visited_x[i] = False
+            visited_y[j] = False
+
+            answer.pop()
+
+choose(1)
+print(maxi)
