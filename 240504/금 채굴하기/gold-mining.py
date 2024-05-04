e3fac2b3a9abb8c2969ade3402005461fd917b2e
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-04 16:29:59 +0900

    Title: 금 채굴하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240504/금 채굴하기/gold-mining.py
+++ 240504/금 채굴하기/gold-mining.py
@@ -8,28 +8,26 @@ n , m = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
 result = 0
+k = 1 
+while 2*(k//2) +1 <= n :
 
-for i in range(n) :
-    for j in range(n) :
-        origin_x, origin_y = i,j
+    cost = - 2*(k**2) -2*k -1
 
-        k = 1 
-        
-        while 2*(k//2) +1 <= n :
+    for i in range(n) :
+        for j in range(n) :
+            origin_x, origin_y = i,j
             total_sum = 0
-            count = 0
-            cost = - 2*(k**2) -2*k -1
+            count = 0  
+
             for x in range(n) :
                 for y in range(n) :
                     distance = abs(x-origin_x) + abs(y-origin_y)
                     if distance <= k and matrix[x][y] == 1 :
                         total_sum += matrix[x][y] * m
                         count += 1
-
-            
             total_cost = total_sum + cost
-            k +=1
             if total_cost >= 0 :
                 result = max(count, result)
+    k+=1
 
 print(result)
