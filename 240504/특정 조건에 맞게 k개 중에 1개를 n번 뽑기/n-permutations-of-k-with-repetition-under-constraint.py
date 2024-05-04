abac9e59ab2a5cc8110cc06c7617bc06998f68f8
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-04 20:21:25 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 358ms, Memory: 29MB, Status: Passed - CodeTree

--- 240504/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240504/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -7,12 +7,12 @@ def print_answer() :
 
 
 def checking(num) :
-    if num == n :
+    if num == n +1 :
         print_answer()
         return
 
     for ele in range(1,k+1) :
-        if num > 3 and answer[-1] == ele and answer[-2] == ele :
+        if num >= 3 and answer[-1] == ele and answer[-2] == ele :
             continue
         answer.append(ele)
         checking(num +1 )
@@ -21,4 +21,4 @@ def checking(num) :
     return
 
 
-checking(0)
+checking(1)
