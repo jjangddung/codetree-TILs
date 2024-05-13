22142891fbfe094fb410a7963b008f08f9396012
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-13 21:49:27 +0900

    Title: 1차원 윷놀이, Time: 5014ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240513/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,47 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n,m, k = map(int, input().split())
+
+
+num_list = list(map(int, input().split()))
+
+
+
+
+
+
+answer = []
+
+
+maxi_count = 0
+
+
+def choose(num) :
+    global maxi_count
+    global m 
+    if num == n +1  :
+        matrix = [0]* (m+1)
+        count  = 0
+        for p in range(n) :
+            j = answer[p]
+            matrix[j] += num_list[p]
+        
+        for mat in matrix :
+            if mat >= m-1 :
+                count +=1
+        
+        maxi_count = max(count,maxi_count)
+
+        return 
+
+    
+    for i in range(n) :
+        answer.append(i)
+        choose(num+1)
+        answer.pop()
+
+choose(1)
+print(maxi_count)
