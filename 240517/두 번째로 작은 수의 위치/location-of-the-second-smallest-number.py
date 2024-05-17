284446e89a48febafd931e47221a9c85e4b013c1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-17 11:13:07 +0900

    Title: 두 번째로 작은 수의 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240517/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+n = int(input())
+
+matrix = list(map(int, input().split()))
+
+new_matrix = matrix.copy()
+
+new_matrix = set(new_matrix)
+new_matrix = list(new_matrix)
+
+new_matrix.sort()
+# print(matrix)
+
+element = new_matrix[1]
+
+if  matrix.count(element) != 1 :
+    print(-1)
+
+else :
+    print(matrix.index(element)+1)
