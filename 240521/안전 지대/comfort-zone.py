03076cba9ef58ce2b95a76efe94b33bfe860fc31
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-05-21 13:00:46 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/안전 지대/comfort-zone.py
+++ 240521/안전 지대/comfort-zone.py
@@ -4,7 +4,7 @@ n ,m  = map(int , input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 result = 1
-result_index = 0
+result_index = 1
 
 def in_range(x,y) :
     return 0 <= x < n and 0 <= y < m 
