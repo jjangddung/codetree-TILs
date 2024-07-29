ce0d848188cbd080f3dc3a97efbb11c26978fc36
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-07-29 18:54:28 +0900

    Title: 최대 증가 부분 수열, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240729/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -9,7 +9,7 @@ matrix = list(map(int, input().split()))
 dp = [0] *n
 
 for i in range(n) :
-    dp[i] = INT_MIN
+    dp[i] = 1
 
 
 dp[0] = 1
@@ -18,14 +18,14 @@ dp[0] = 1
 for i in range(1,n) :
     for j in range(i) :
 
-        if dp[j] == INT_MIN :
-            continue
-
         if matrix[i] > matrix[j] :
-            dp[i] = max(dp[i], dp[j] + 1,1)
+            dp[i] = max(dp[i], dp[j] + 1)
+
+    
 
 
 
 
 
+# print(*dp)
 print(max(dp))
