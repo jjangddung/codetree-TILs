7f5c8b97c36a119a6b30d0342ada083042cb5950
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-07-29 18:46:43 +0900

    Title: 최대 증가 부분 수열, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,29 @@
+import sys
+INT_MIN = -sys.maxsize
+
+n = int(input())
+
+
+matrix = list(map(int, input().split()))
+
+dp = [0] *n
+
+for i in range(n) :
+    dp[i] = INT_MIN
+
+
+dp[0] = 1
+
+
+for i in range(1,n) :
+    for j in range(i) :
+
+        if matrix[j] == INT_MIN :
+            continue
+
+        if matrix[i] >= matrix[j] :
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+
+print(max(dp))
