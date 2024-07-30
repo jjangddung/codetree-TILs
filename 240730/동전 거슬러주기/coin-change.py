f7dfb25c1e22458c5201344276adc9c38ebbb9fb
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-07-30 23:58:37 +0900

    Title: 동전 거슬러주기, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/동전 거슬러주기/coin-change.py
+++ 240730/동전 거슬러주기/coin-change.py
@@ -1,35 +1,27 @@
-# dp[i]의 정의  합이 i일 때, 최소한의 동전 개수
-# 합 i를 만들기 전 사용한 동전이 j번째라면,,, dp[i-matrix[j]] + 1
-
-
 import sys
 
-INT_MIN = sys.maxsize
-n,m = map(int, input().split())
+INT_MAX = sys.maxsize
+n, m = map(int, input().split())
 
+matrix = list(map(int, input().split()))
 
-matrix= list(map(int, input().split()))
+dp = [INT_MAX] * (m + 1)
+dp[0] = 0  # 합이 0일 때는 동전이 필요하지 않음
 
+for i in range(n):
+    coin = matrix[i]
+    if coin <= m:
+        dp[coin] = 1
 
-dp = [INT_MIN]* (m+1)
-
-
-
-
-for i in range(1,m+1) :
-    for j in range(n) :
-        if i >= matrix[j] :
-            if dp[i- matrix[j]] == INT_MIN :
-                continue
-
-            dp[i] = min(dp[i], dp[i- matrix[j]] +1 )
-
-
-
+for i in range(1, m + 1):
+    for j in range(n):
+        if i >= matrix[j]:
+            if dp[i - matrix[j]] != INT_MAX:
+                dp[i] = min(dp[i], dp[i - matrix[j]] + 1)
 
 ans = dp[m]
 
-if ans == INT_MIN :
+if ans == INT_MAX:
     ans = -1
 
 print(ans)
