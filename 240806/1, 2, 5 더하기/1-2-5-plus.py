3ebf03e6728b2ac58ea1e69ae65ba4def89301d5
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-06 14:17:51 +0900

    Title: 1, 2, 5 더하기, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240806/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,26 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+matrix = [1, 2, 5]
+coin = [1 ,2,9]
+n = int(input())
+
+dp = [INT_MIN] * (n + 1)
+
+# 초기 값 설정
+dp[0] = 1
+
+
+# DP 배열 업데이트
+for i in range(1, n + 1):
+    for j in range(3):
+        if dp[i-matrix[j]] != INT_MIN :
+            if j == 0  :
+                dp[i] = 0
+
+            dp[i] += dp[i-matrix[j]]
+
+
+# 결과 출력
+print(dp[n])
