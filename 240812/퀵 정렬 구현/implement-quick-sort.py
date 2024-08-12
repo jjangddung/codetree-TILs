153b9416ed6bf064ff5b0750be4add6f247ec811
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-12 18:53:13 +0900

    Title: 퀵 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240812/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+
+matrix = list(map(int, input().split()))
+
+
+# pivot 값을 정하고 그 값을 기준으로  나눠야한다.
+# 데이터가 3개 이하면 피벗은 반드시 마지막 값이 됨
+# 데이터가 4개 이상이면 맨 왼쪽, 오른쪽, 가운데 (나머지 버림) 값 중 중간 값을 선택함
+# 피벗이 선택되면 먼저 구간의 맨 끝 원소와 꼭 교환해야함
+
+
+def quick_sort(arr,start,end) :
+    if len(arr) == 1 :
+        return arr
+    
+    pivot = end
+    new_arr1 = []
+    new_arr2 = []
+    for i in range(len(arr)-1) :
+        if arr[i] < pivot :
+            new_arr1.append(arr[i])
+        else :
+            new_arr2.append(arr[i])
+    
+
+    return quick_sort(new_arr1) + [pivot] + quick_sort(new_arr2)
+
+
+print(quick_sort(matrix,0,n-1))
