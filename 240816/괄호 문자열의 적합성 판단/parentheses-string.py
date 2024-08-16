7d080315f745bfc5ca8bdcbad365d42242824b11
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-16 14:23:49 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,53 @@
+class stack :
+    def __init__(self) :
+        self.items = []
+    
+    def push(self, item) :
+        self.items.append(item)
+    
+    def pop(self,item) :
+        self.items.pop()
+    
+    def empty(self) :
+        if len(self.items)== 0 :
+            return True
+        
+        else :
+            return False
+
+    def top(self) :
+        if not empty():
+            return self.items[-1]
+    
+    def size(self) :
+
+        return len(self.items)
+
+
+
+front = stack()
+# back = stack() 
+
+sign = list(map(str, input().split()))
+count = 0
+for i in range(len(sign)) :
+    if i == 0 :
+        if sign[i] == ')' :
+            print('No')
+            count +=1
+            break
+    
+    if sign[i] == '(' :
+        front.push('(')
+    
+    else :
+        if not front.empty() :
+            front.pop()
+        
+        else :
+            print('No')
+            count +=1
+            break
+
+if count == 0 :
+    print('Yes')
