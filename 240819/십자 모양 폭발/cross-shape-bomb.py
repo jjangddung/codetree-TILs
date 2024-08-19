e6c500b32bb2186833c3a0853acb177f9b8adfd5
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-19 15:21:37 +0900

    Title: 십자 모양 폭발, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240819/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,45 @@
+import sys
+
+n = int(input())
+
+grid = [list(map(int , input().split())) for _ in range(n)]
+
+r,c = map(int, input().split())
+
+r-=1
+c-=1
+
+dxs, dys = [-1,0,1,0], [0,1,0,-1] # 상우하좌
+
+def can_go(x,y) :
+    return  0 <= x < n and 0 <= y < n
+
+
+def boomb(r,c) :
+    num = grid[r][c]
+    grid[r][c] = 0
+
+    count = num-1
+
+    for dx, dy in zip(dxs,dys) :
+        d = 1
+        while d <= count :
+            new_x, new_y = r + d*dx , c + d*dy
+            if can_go(new_x,new_y) :
+                grid[new_x][new_y] = 0
+                d +=1
+            else :
+                break
+
+
+def filler() :
+    for i in range(n) :
+        for j in range(n-1) :
+            if grid[j][i] != 0 and grid[j+1][i] == 0 :
+                grid[j][i],grid[j+1][i] = grid[j+1][i] ,grid[j][i]
+
+
+boomb(r,c)
+filler()
+for _ in grid:
+    print(*_)
