a6cea9d39b75b439c9919aeb2694b478fddd1f3b
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-20 22:54:42 +0900

    Title: 1차원 폭발 게임, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240820/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240820/1차원 폭발 게임/The-1D-bomb-game.py
@@ -56,14 +56,18 @@ def group(grid) :
 
 answer = group(grid)
 # print(answer[0])
-while  answer[1] != 1 :
-    # print("list: ", answer[0])
-    answer = group(answer[0])
 
-
-# print("answer: ", answer)
-
-result = answer[0]
-print(len(result))
-for _ in result :
-    print(_)
+if m == 1 :
+    print(0)
+else :
+    while  answer[1] != 1 :
+        print("list: ", answer[0])
+        answer = group(answer[0])
+    
+    
+    # print("answer: ", answer)
+    
+    result = answer[0]
+    print(len(result))
+    for _ in result :
+        print(_)
