3d273112e5c65592d0265cec2f6ae634c56f2e7f
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-08-26 23:44:04 +0900

    Title: 대각선으로 숫자 채우기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240826/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240826/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -7,22 +7,22 @@ n,m = map(int, input().split())
 
 grid = [[0]*m for _ in range(n)]
 
+#대각선의 개수를 알면 되는 문제
 
 
-if m == 1 :
-    for i in range(1,n+1) :
-        print(i)
 
 
-else :
-    num= 1
-    for total in range(2*m) :
-        for j in range(total+1) :
-            try :
-                grid[j][total-j] = num
-                num +=1
-            except :
-                pass
-    
-    for g in grid :
-        print(*g)
+
+
+
+
+num= 1
+for total in range(n+m-1) :
+    for j in range(total+1) :
+        try :
+            grid[j][total-j] = num
+            num +=1
+        except :
+            pass
+for g in grid :
+    print(*g)
