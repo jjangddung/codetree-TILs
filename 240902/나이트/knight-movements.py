1c4303c28f64a013bbb9c2292f2e0918b542eccf
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-02 17:07:17 +0900

    Title: 나이트, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240902/나이트/knight-movements.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+
+
+n = int(input())
+
+q = deque()
+
+step = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
+
+def in_range(x,y) :
+    return 0 <= x < n and 0 <= y < n
+
+
+
+
+
+def push(x,y,s) :
+    step[x][y] = s
+    visited[x][y] = True
+    q.append((x,y))
+
+
+def bfs() :
+    dxs = [-2,-1,1,2,2,1,-1,-2]  # 나이트의 움직임
+    dys = [1,2,2,1,-1,-2,-2,-1]
+
+    while q :
+        x,y = q.popleft()
+
+        for dx, dy in zip(dxs,dys) :
+            new_x,new_y = x +dx , y +dy
+
+            if in_range(new_x,new_y) and not visited[new_x][new_y] :
+                push(new_x,new_y,step[x][y] +1)
+    
+
+r1,c1,r2,c2 = map(int, input().split())
+
+push(r1-1,c1-1,0)
+bfs()
+
+result = step[r2-1][c2-1]
+
+# print(*step)
+
+if result == 0 :
+    print(-1)
+
+else :
+    print(result)
