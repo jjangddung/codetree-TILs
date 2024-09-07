6f3eb75097d241d7851df79cae5e471c99b98636
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-07 19:47:42 +0900

    Title: n개의 점 중 m개 고르기, Time: 384ms, Memory: 31MB, Status: Passed - CodeTree

--- 240907/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240907/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -40,14 +40,20 @@ result = sys.maxsize
 
 for ans in ans_lst :
     length = len(ans)
+    # print("length: ",length)
+    # print("ans: ", ans)
     semi = 0
 
     for i in range(length) :
-        for j in range(i, length) :
-            p1  = grid[i]
-            p2 =  grid[j]
+        for j in range(i+1, length) :
+            p1  = grid[ans[i]]
+            p2 =  grid[ans[j]]
+            # print("p1: ",p1)
+            # print("p2: ",p2)
             distance = (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+            # print(distance)
             semi = max(distance,semi)
+            
     result = min(semi,result)
             
 
