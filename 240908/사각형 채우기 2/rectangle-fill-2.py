baffced225baa01651b37e2440f8f82e9c8b5f15
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-08 11:40:57 +0900

    Title: 사각형 채우기 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/사각형 채우기 2/rectangle-fill-2.py
+++ 240908/사각형 채우기 2/rectangle-fill-2.py
@@ -6,13 +6,13 @@ dp = [0]*(1001)
 dp[0] = 1
 dp[1] = 1
 dp[2] = 3
-dp[3] = dp[2] + 3 *dp[1]
+# dp[3] = dp[2] + 3 *dp[1]
 
 # dp[i] = dp[i-1] + 3*dp[i-2]
 
 
 for i in range(3,n+1) :
-    dp[i] = dp[i-1] + 3*dp[i-2]
+    dp[i] = dp[i-1] + 2*dp[i-2]
     dp[i] = dp[i]%10007
 
 print(dp[n])
