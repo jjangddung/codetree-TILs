0b80b76da9b7650d384a493c365ea91d71435534
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-09 23:19:20 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 210ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240909/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240909/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -40,13 +40,13 @@ def checking(x,y) :
 
 def initialize(mini) :
     for i in range(1,n) :
-        if grid[0][i] > mini :
+        if grid[0][i] >= mini :
             dp[0][i] = max(dp[0][i-1],grid[0][i])
         
         else :
             dp[0][i] = 127
         
-        if grid[i][0] > mini :
+        if grid[i][0] >= mini :
             dp[i][0] = max(dp[i-1][0],grid[i][0])
         
         else :
@@ -76,9 +76,14 @@ for mini in range(maximum,minimum-1,-1) :
 
     dp[0][0] = grid[0][0]
 
+    # if mini == 2 :
+        # for d in dp :
+            # print(*d)
+
+
     initialize(mini)
 
-    # if mini == 20 :
+    # if mini == 2 :
         # for d in dp :
             # print(*d)
 
@@ -101,7 +106,7 @@ for mini in range(maximum,minimum-1,-1) :
             # if mini == 20 :
                 # print(f'i:{i},j:{j} ,dp: {dp[i][j]}')
     
-    # if mini == 20 :
+    # if mini == 2 :
         # for d in dp :
             # print(*d)
     # 
