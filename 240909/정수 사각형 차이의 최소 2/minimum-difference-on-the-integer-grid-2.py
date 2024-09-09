b42d51a4c5a5029ba16fff2d2c1246e1121096a1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-09 23:09:15 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 221ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240909/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240909/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -74,21 +74,42 @@ for mini in range(maximum,minimum-1,-1) :
             else :
                 dp[i][j] = -1
 
+    dp[0][0] = grid[0][0]
+
     initialize(mini)
 
-    for i in range(n) :
-        for j in range(n) :
+    # if mini == 20 :
+        # for d in dp :
+            # print(*d)
 
-            if grid[i][j] == 127 :
-                dp[i][j] == 127
+    
+
+    for i in range(1,n) :
+        for j in range(1,n) :
+
+            if dp[i][j] == 127 :
+                continue
+
+
+            if dp[i-1][j] == dp[i][j-1] and dp[i-1][j] == 127 :
+                dp[i][j] = 127
                 continue
             dp[i][j] = min(dp[i-1][j],dp[i][j-1])
             
             dp[i][j] = max(dp[i][j],grid[i][j])
+
+            # if mini == 20 :
+                # print(f'i:{i},j:{j} ,dp: {dp[i][j]}')
     
+    # if mini == 20 :
+        # for d in dp :
+            # print(*d)
+    # 
     if mini > dp[n-1][n-1] :
         continue
 
-    final = min(final,dp[n-1][n-1]-mini)
+    result = dp[n-1][n-1] - mini
+    final = min(final,result)
+    # print(mini,result,final)
 
 print(final)
