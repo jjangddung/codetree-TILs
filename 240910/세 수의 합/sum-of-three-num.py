fd7f5f7978e62de79c25be6026bd77295d3f5cc1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-10 20:56:52 +0900

    Title: 세 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240910/세 수의 합/sum-of-three-num.py
+++ 240910/세 수의 합/sum-of-three-num.py
@@ -33,13 +33,19 @@ for i in d :
 
             # print(i,j,k)
             if i == j and j != k :
+                if d[i] < 2 :
+                    continue
                 count += d[k]*d[i]*(d[i]-1)//2
                 # print("1: ")
             
             elif i != j and j == k :
+                if d[k] < 2:
+                    continue
                 count += d[i]*d[j]*(d[j]-1)//2
             
             elif i == j ==  k :
+                if d[i] < 3 :
+                    continue
                 count += d[i]*d[i-1]*d[i-2]//6
             
             else :
