80c1098073ba4520686c4ee3e173127925828f66
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-10 20:58:25 +0900

    Title: 세 수의 합, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240910/세 수의 합/sum-of-three-num.py
+++ 240910/세 수의 합/sum-of-three-num.py
@@ -1,56 +1,30 @@
-import sys
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-n,m = map(int, input().split())
+count = dict()
 
-grid = list(map(int, input().split()))
+# 각 숫자가 몇 번씩 나왔는지를
+# hashmap에 기록해줍니다.
+for elem in arr:
+    if elem in count:
+        count[elem] += 1
+    else:
+        count[elem] = 1
 
-grid.sort()
+ans = 0
+# 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+for i in range(n):
+    # 이미 순회한 적이 있는 숫자는 빼 버림으로서
+    # 같은 조합이 여러번 세어지는 걸 방지합니다.
+    count[arr[i]] -= 1
 
-d = {}
+    for j in range(i):
+        # 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
+        # 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
+        diff = k - arr[i] - arr[j]
 
-for i in range(n) :
-    v = grid[i]
+        if diff in count:
+            ans += count[diff]
 
-    if v not in d :
-        d[v] =1
-    
-    else :
-        d[v] +=1
-
-
-# print(d)
-# i,j,k 세 가지 value 뽑기
-count = 0
-for i in d :
-    for j in d :
-        if j < i :
-            continue
-        for k in d :
-            if k < j :
-                continue
-            if i + j + k  != m :
-                continue
-
-            # print(i,j,k)
-            if i == j and j != k :
-                if d[i] < 2 :
-                    continue
-                count += d[k]*d[i]*(d[i]-1)//2
-                # print("1: ")
-            
-            elif i != j and j == k :
-                if d[k] < 2:
-                    continue
-                count += d[i]*d[j]*(d[j]-1)//2
-            
-            elif i == j ==  k :
-                if d[i] < 3 :
-                    continue
-                count += d[i]*(d[i]-1)*(d[i]-2)//6
-            
-            else :
-                count += d[i]*d[j]*d[k]
-            # print(count)
-
-
-print(count)
+print(ans)
