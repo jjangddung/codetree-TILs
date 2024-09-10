92c530722f845ecfe27730338da9b710d1e9b957
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-10 13:08:05 +0900

    Title: 2차원 최대 증가 수열, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 240910/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240910/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -24,14 +24,17 @@ dp[0][0] = 1
 
 count = 1
 for i in range(n) :
-    for j in range(n):
+    for j in range(m):
         for p in range(i) :
             for q in range(j) :
                 if dp[p][q] == -1 :
                     continue
                 if grid[p][q] < grid[i][j] :
                     dp[i][j] = max(dp[p][q]+1, dp[i][j])
+                    # print(i,j,p,q)
         
         count = max(count, dp[i][j])
 
+
+# print(*dp, sep = "\n")
 print(count)
