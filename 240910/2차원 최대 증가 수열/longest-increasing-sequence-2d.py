2b2bed15261df8aafe4271a78a62e6278b265211
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-10 12:59:48 +0900

    Title: 2차원 최대 증가 수열, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240910/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,37 @@
+import sys
+
+n,m = map(int, input().split())
+
+
+# 체킹된 좌표보다 낮은 좌표 한해서 출발 시켜야함
+
+
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+dp = [
+    [-1]*m
+    for _ in range(n)
+]
+
+
+
+dp[0][0] = 1
+
+count = 1
+for i in range(n) :
+    for j in range(n):
+        for p in range(i) :
+            for q in range(j) :
+                if dp[p][q] == -1 :
+                    continue
+                if grid[p][q] < grid[i][j] :
+                    dp[i][j] = max(dp[p][q]+1, dp[i][j])
+        
+        count = max(count, dp[i][j])
+
+print(count)
