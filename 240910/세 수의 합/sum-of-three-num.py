8af9e465f90fc937fef2c7a6d2f92b826aeaf92f
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-10 20:55:43 +0900

    Title: 세 수의 합, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/세 수의 합/sum-of-three-num.py
@@ -0,0 +1,50 @@
+import sys
+
+n,m = map(int, input().split())
+
+grid = list(map(int, input().split()))
+
+grid.sort()
+
+d = {}
+
+for i in range(n) :
+    v = grid[i]
+
+    if v not in d :
+        d[v] =1
+    
+    else :
+        d[v] +=1
+
+
+print(d)
+# i,j,k 세 가지 value 뽑기
+count = 0
+for i in d :
+    for j in d :
+        if j < i :
+            continue
+        for k in d :
+            if k < j :
+                continue
+            if i + j + k  != m :
+                continue
+
+            # print(i,j,k)
+            if i == j and j != k :
+                count += d[k]*d[i]*(d[i]-1)//2
+                # print("1: ")
+            
+            elif i != j and j == k :
+                count += d[i]*d[j]*(d[j]-1)//2
+            
+            elif i == j ==  k :
+                count += d[i]*d[i-1]*d[i-2]//6
+            
+            else :
+                count += d[i]*d[j]*d[k]
+            # print(count)
+
+
+print(count)
