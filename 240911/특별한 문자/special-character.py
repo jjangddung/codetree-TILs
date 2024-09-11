0a9486f69179a3f20b6a696752d0763d44146f18
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-11 12:58:14 +0900

    Title: 특별한 문자, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/특별한 문자/special-character.py
@@ -0,0 +1,27 @@
+import sys
+
+
+alpha = str(input())
+
+
+d = {}
+
+for i in range(len(alpha)) :
+    if alpha[i] not in d :
+        d[alpha[i]] = 1
+    
+    else :
+        d[alpha[i]] +=1
+    
+
+total = []
+
+for v in d :
+    if d[v] == 1:
+        total.append(v)
+
+if len(total) != 0 :
+    print(*total, sep = " ")
+
+else :
+    print("None")
