257c63af810f8a889afc54c490b528ae71995bfb
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-14 22:29:03 +0900

    Title: 상해버린 치즈, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240914/상해버린 치즈/rotten-cheese.py
+++ 240914/상해버린 치즈/rotten-cheese.py
@@ -28,16 +28,18 @@ for _ in range(S) :
     
 
 
-count = 0
+
 # print(cons_lst)
 cons_lst = set(cons_lst)
 cons_lst = list(cons_lst)
 # print(cons_lst)
 human = []
-
+maxi = 0
 for ans in cons_lst :
+    count = 0
     count_lst  = 0
     new_lst = []
+    check_lst= []
     for case in cheese_lst :
         if ans == case[1]  :
             new_lst.append(case[0])
@@ -46,12 +48,23 @@ for ans in cons_lst :
     new_lst = set(new_lst)
     new_lst = list(new_lst)
     if len(new_lst) >= S :
+        # print(ans)
         
         for t in range(len(new_lst)) :
-            human.append(new_lst[t])
+            check_lst.append(new_lst[t])
+            # human.append(new_lst[t])
+
+    for t in sick_lst :
+        if t not in check_lst :
+            count +=1
+            break
+    if count != 0 :
+        continue
+    # print("human: ",len(check_lst))
+    maxi = max(maxi,len(check_lst))
             
 
 
-human = set(human)
+# human = set(human)
 
-print(len(human))
+print(maxi)
