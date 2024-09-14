e0902d73d0fdbe60e97599ee69daa44d811646e7
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-14 22:13:54 +0900

    Title: 상해버린 치즈, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240914/상해버린 치즈/rotten-cheese.py
+++ 240914/상해버린 치즈/rotten-cheese.py
@@ -16,8 +16,10 @@ for _ in range(D) :
     cheese_lst.append([p,m,t])
 
 cons_lst = []
+sick_lst = []
 for _ in range(S) :
     p,t = map(int, input().split())
+    sick_lst.append(p)
     
 
     for case in cheese_lst :
@@ -34,9 +36,21 @@ cons_lst = list(cons_lst)
 human = []
 
 for ans in cons_lst :
+    count_lst  = 0
+    new_lst = []
     for case in cheese_lst :
-        if ans == case[1] :
-            human.append(case[0])
+        if ans == case[1]  :
+            new_lst.append(case[0])
+    
+    # print(new_lst)
+    new_lst = set(new_lst)
+    new_lst = list(new_lst)
+    if len(new_lst) >= S :
+        
+        for t in range(len(new_lst)) :
+            human.append(new_lst[t])
+            
+
 
 human = set(human)
 
