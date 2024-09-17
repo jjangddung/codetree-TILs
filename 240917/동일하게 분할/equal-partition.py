790aaaea0c5e7068e890e251d50bc28a3c221a43
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-17 16:08:31 +0900

    Title: 동일하게 분할, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/동일하게 분할/equal-partition.py
+++ 240917/동일하게 분할/equal-partition.py
@@ -1,56 +1,50 @@
 import sys
 
+
 INT_MIN = -sys.maxsize
 
 n = int(input())
 
-arr =  [0]  +list(map(int, input().split()))
+grid = [0] + list(map(int, input().split()))
 
 
-OFFSET = 10000
+OFFSET = 1000
 
-m = sum(arr)
-
+m = sum(grid)
 
 dp = [
-    [0]*(m+ 1+OFFSET)
+    [0]*(m+1+OFFSET)
 
     for _ in range(n+1)
 ]
 
+def init() :
+    for i in range(n+1) :
+        for j in range(-m,m+1) :
+            dp[i][j+ OFFSET] =  INT_MIN
 
-def initialize():
-    # 최대를 구하는 문제이므로
-    # 초기값을 INT_MIN으로 넣어줍니다.
-    for i in range(n + 1):
-        for j in range(-m, m + 1):
-            dp[i][j + OFFSET] = INT_MIN
-
-    # 초기 조건은
-    # 아직 아무런 수도 고른적이 없는 경우이므로 
-    # 0번째 수까지 고려하여
-    # 그룹 A 합 - 그룹 B 합이 0이고 
-    # 그룹 A의 합이 0인 경우에 대한 정보 입니다.
-    dp[0][0 + OFFSET] = 0
-
-
-
-initialize()
-
-def update(i, j, prev_i, prev_j, val):
-    # 불가능한 경우 패스합니다.
-    if prev_j < -m or prev_j > m or dp[prev_i][prev_j + OFFSET] == INT_MIN:
-        return
     
-    dp[i][j + OFFSET] = max(dp[i][j + OFFSET], dp[prev_i][prev_j + OFFSET] + val)
+    dp[0][0 + OFFSET] = INT_MIN
 
+
+
+def update(i,j,prev_i,prev_j,val) :
+
+    if prev_j < -m or prev_j > m or dp[prev_i][prev_j] == INT_MIN :
+        return 
+    
+    dp[i][j+ OFFSET] = max(dp[i][j+OFFSET], dp[prev_i][prev_j+ OFFSET] + val)
+
+
+
+init()
 
 for i in range(1,n+1) :
     for j in range(-m,m+1) :
+        
 
-        update(i,j,i-1,j-arr[i],arr[i])
-
-        update(i,j,i-1,j+arr[i],0)
+        update(i,j,i-1,j-grid[i],grid[i])
+        update(i,j, i-1,j + grid[i], 0)
 
 
 if dp[n][0+OFFSET] > 0 :
