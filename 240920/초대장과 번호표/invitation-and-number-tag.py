9964b7d58a50638bdb231cd6f216d4acd2375a42
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-20 14:33:32 +0900

    Title: 초대장과 번호표, Time: 1404ms, Memory: 29MB, Status: Passed - CodeTree

--- 240920/초대장과 번호표/invitation-and-number-tag.py
+++ 240920/초대장과 번호표/invitation-and-number-tag.py
@@ -2,22 +2,38 @@ n, g = map(int, input().split())
 
 invite = set()
 invite.add(1)
+value = 0
+t= 0
 
+g_lst = []
 
 for i in range(g) :
     lst = list(map(int, input().split()))
-    num = lst[0]
-    member = lst[1:]
+    g_lst.append(lst)
 
-    count = 0
+while True :
+    some = 0
+    for i in range(g) :
+        lst = g_lst[i]
+        
+        num = lst[0]
+        member = lst[1:]
 
-    for m in member :
-        if m in invite :
-            count +=1
-        else :
-            not_inv = m
+        count = 0
+
+        for m in member :
+            if m in invite :
+                count +=1
+            else :
+                not_inv = m
+
+        if num - count == 1 :
+            some +=1
+            invite.add(not_inv)
     
-    if num - count == 1 :
-        invite.add(not_inv)
+    if some == 0 :
+        break
+
+
 
 print(len(invite))
