d43b845ad7e0e97e5181eb1a8ebb6e8923f7da84
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-22 18:33:39 +0900

    Title: 계단 오르기 2, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240922/계단 오르기 2/climbing-stairs-2.py
+++ 240922/계단 오르기 2/climbing-stairs-2.py
@@ -36,7 +36,7 @@ for i in range(1,n+1) :
     for j in range(4) :
         if not in_range(i-1,j-1) :
             continue 
-        print(i,j)
+        # print(i,j)
         dp[i][j] = max(dp[i-1][j-1] + grid[i], dp[i][j])
 
         if not in_range(i-2,j) :
