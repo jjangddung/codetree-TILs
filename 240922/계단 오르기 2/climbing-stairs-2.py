35e391190db239699ace9822781ae497259d92e5
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-22 18:47:35 +0900

    Title: 계단 오르기 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240922/계단 오르기 2/climbing-stairs-2.py
+++ 240922/계단 오르기 2/climbing-stairs-2.py
@@ -11,16 +11,19 @@ dp = [[INT_MIN] * 4 for _ in range(n + 1)]
 for j in range(4):
     dp[0][j] = 0
 
-def in_range(x, y):
+def odd_in_range(x, y):
+    return 0 <= x <= n and 0 <= y < 3
+
+def even_in_range(x, y):
     return 0 <= x <= n and 0 <= y < 4
 
 maxi = INT_MIN
 for i in range(1, n + 1):
     for j in range(4):
-        if in_range(i - 1, j - 1) and dp[i - 1][j - 1] != INT_MIN:
+        if odd_in_range(i - 1, j - 1) and dp[i - 1][j - 1] != INT_MIN:
             dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + grid[i])
 
-        if in_range(i - 2, j) and dp[i - 2][j] != INT_MIN:
+        if even_in_range(i - 2, j) and dp[i - 2][j] != INT_MIN:
             dp[i][j] = max(dp[i][j], dp[i - 2][j] + grid[i])
 
         maxi = max(maxi, dp[i][j])
