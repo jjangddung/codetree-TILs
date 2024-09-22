75fb11de6d7e4c3510ea1500c05eb42cb1148b48
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-22 18:45:22 +0900

    Title: 계단 오르기 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240922/계단 오르기 2/climbing-stairs-2.py
+++ 240922/계단 오르기 2/climbing-stairs-2.py
@@ -1,49 +1,28 @@
 import sys
 
-n= int(input())
-
+n = int(input())
 INT_MIN = -sys.maxsize
 grid = [0] + list(map(int, input().split()))
 
+# dp[i][j]: i번째까지의 계단 도달 시 j번만큼 1칸씩 움직였을 때의 최대 동전 개수
+dp = [[INT_MIN] * 4 for _ in range(n + 1)]
 
+# 초기값 설정
+for j in range(4):
+    dp[0][j] = 0
 
-# dp[i][j] i번째까지의 계단도달 >>> (무조건 도달임) 이때   j번만큼 1칸씩 움직였을 때의 최대 동전 개수
+def in_range(x, y):
+    return 0 <= x <= n and 0 <= y < 4
 
-# 한 칸씩만 움직이는 경우 dp[i][j] = max(dp[i-1][j-1] + grid[i], dp[i][j])
-# 두 칸씩만 움직이는 경우 dp[i][j] = max(dp[i-2][j] + grid[i], dp[i][j])
+maxi = INT_MIN
+for i in range(1, n + 1):
+    for j in range(4):
+        if in_range(i - 1, j - 1) and dp[i - 1][j - 1] != INT_MIN:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + grid[i])
 
-
-
-
-dp = [
-    [INT_MIN]*(4)
-    for _ in range(n+1) 
-]
-
-
-for i in range(4) :
-    dp[0][i] = 0
-
-
-def in_range(x,y) :
-    return 0 <= x <= n and 0 <= y < 4 
-
-# print(grid)
-# print(dp)
-# print(dp[4][1])
-maxi = -sys.maxsize
-for i in range(1,n+1) :
-    for j in range(4) :
-        if  in_range(i-1,j-1) and dp[i-1][j-1] != INT_MIN  :
-            # continue 
-        # print(i,j)
-            dp[i][j] = max(dp[i-1][j-1] + grid[i], dp[i][j])
-
-        if  in_range(i-2,j) and dp[i-2][j] != INT_MIN :
-            dp[i][j] = max(dp[i-2][j] + grid[i], dp[i][j])
+        if in_range(i - 2, j) and dp[i - 2][j] != INT_MIN:
+            dp[i][j] = max(dp[i][j], dp[i - 2][j] + grid[i])
 
         maxi = max(maxi, dp[i][j])
 
-
-# print(dp)
 print(maxi)
