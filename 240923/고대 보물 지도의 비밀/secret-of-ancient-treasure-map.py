e4e64180c2cf2108a5e52ee43288d26231b017ff
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 22:04:17 +0900

    Title: 고대 보물 지도의 비밀, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 240923/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -5,31 +5,29 @@ n,m = map(int, input().split())
 
 grid = [0] +  list(map(int, input().split()))
 
-# dp[i][j]  i개를 연속해서 골랐을 때, 음수의 개수가 j개일때의 최대값
+inf =-sys.maxsize
 
-dp = [
-    [-1]*(m+1)
-    for _ in range(n+1)
-]
+dp = [[[inf for _ in range(m+1)] for _ in range(n+1)] for _ in range(n+1)]
 
-
-dp[0][0] = 0
+dp[0][0][0] = 0
 
 maxi = -sys.maxsize
+
 for i in range(1,n+1) :
-    for j in range(m+1) :
-        for k in range(1,n+2-i): 
-            new_lst = grid[k:i+k]
-            count = 0
-            result = 0
-            for v in new_lst :
-                if v < 0 :
-                    count +=1
-                result += v
+    for j in range(1,n+1) :
+        for k in range(m+1) :
+            if grid[j] < 0 :
+                if k-1 >= 0 :
+                    if dp[i-1][j-1][k-1] == inf :
+                        continue
+                    dp[i][j][k] = max(dp[i-1][j-1][k-1] + grid[j], dp[i][j][k])
             
-            if count == j :
-                dp[i][j] = max(dp[i][j], result)
-                maxi = max(dp[i][j],maxi)
+            else :
+                if dp[i-1][j-1][k] == inf :
+                        continue
+                dp[i][j][k] = max(dp[i-1][j-1][k] + grid[j], dp[i][j][k])
+            
+            maxi = max(dp[i][j][k], maxi)
 
 print(maxi)
 # print(dp)
