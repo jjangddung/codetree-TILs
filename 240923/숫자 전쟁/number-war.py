f287f06371e48c4a172f54da8c2eb66c094af940
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 19:36:11 +0900

    Title: 숫자 전쟁, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/숫자 전쟁/number-war.py
+++ 240923/숫자 전쟁/number-war.py
@@ -19,36 +19,25 @@ for i in range(n+1) :
 maxi = 0
 for i in range(1,n+1) :
     for j in range(1,n+1) :
-
-        if b[i-1] <= a[j] :
+        if b[i-1] < a[j] :
             dp[i][j] = max(dp[i-1][j],dp[i][j])
         
-        if b[i]  >= a[j-1] :
+        if b[i]  > a[j-1] :
             dp[i][j] = max(dp[i][j-1],dp[i][j])
-
         
-        
-
         dp[i][j] = max(dp[i-1][j-1],dp[i][j])
+        
+        if a[j] > b[i] :
+            
+            dp[i][j] = dp[i][j] + b[i]
 
-        if b[i-1] == a[j-1] :
-            dp[i][j] = dp[i-1][j-1]
+            if i == n :  
+                maxi = max(maxi, dp[i][j])
         
         else :
-            if a[j] > b[i] :
-                dp[i][j] = dp[i][j] + b[i]
-
-                if i == n :
-                    maxi = max(maxi, dp[i][j])
-            
-            elif a[j] < b[i] :
-                if j == n :
-                    maxi = max(maxi, dp[i][j])
+            if i==n or j  == n :
+                maxi = max(maxi, dp[i][j])
 
 
-        
-        
 
-
-# print(dp)
 print(maxi)
