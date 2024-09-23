61b4e75ff4d3df78100535751a677eece603dce1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 19:53:53 +0900

    Title: 숫자 전쟁, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/숫자 전쟁/number-war.py
+++ 240923/숫자 전쟁/number-war.py
@@ -19,25 +19,43 @@ for i in range(n+1) :
 maxi = 0
 for i in range(1,n+1) :
     for j in range(1,n+1) :
-        if b[i-1] <= a[j] :
+        if b[i-1] < a[j] :
+            if dp[i-1][j] == -1 :
+                continue
             dp[i][j] = max(dp[i-1][j],dp[i][j])
-        
-        if b[i]  >= a[j-1] :
+
+        elif b[i-1] == a[j] :
+            dp[i][j] =-1
+            continue
+
+        if b[i] > a[j-1] :
+            if dp[i][j-1] == -1 :
+                continue
+                
             dp[i][j] = max(dp[i][j-1],dp[i][j])
-        
-        dp[i][j] = max(dp[i-1][j-1],dp[i][j])
-        
-        if a[j] > b[i] :
-            
+
+        elif b[i] == a[j-1] :
+            dp[i][j] = -1
+            continue
+
+        if b[i] < a[j] :
             dp[i][j] = dp[i][j] + b[i]
 
-            if i == n :  
-                maxi = max(maxi, dp[i][j])
+            if i == n :
+                maxi = max(dp[i][j], maxi)
+            
+        elif b[i] > a[j] :
+            if j == n :
+                maxi = max(dp[i][j], maxi)
         
         else :
-            if i==n or j  == n :
-                maxi = max(maxi, dp[i][j])
+            if j == n or i == n :
+                maxi = max(dp[i][j], maxi)
 
+        
+        
 
-
+    
 print(maxi)
+print(dp)
+#
