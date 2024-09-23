1770f8ff5f63c4e9c3c0c631f34444790334ca34
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 20:47:19 +0900

    Title: 숫자 전쟁, Time: 171ms, Memory: 30MB, Status: Passed - CodeTree

--- 240923/숫자 전쟁/number-war.py
+++ 240923/숫자 전쟁/number-war.py
@@ -1,55 +1,36 @@
-import sys
-
+# 각 플레이어의 카드 정보를 입력받습니다.
 n = int(input())
+a = [0] + list(map(int, input().split())) + [0]
+b = [0] + list(map(int, input().split())) + [0]
 
-a = [0] +  list(map(int, input().split()))
-b = [0] +  list(map(int, input().split()))
+# dp 배열을 초기화합니다. 초기값은 -1로 설정합니다.
+dp = [[-1 for _ in range(n + 5)] for _ in range(n + 5)]
 
+# 기본 케이스를 설정합니다.
+dp[0][0] = 0
 
-dp = [
-    [-1]*(n+1)
-    for _ in range(n+1)
-]
+# 각 경우의 수를 동적 프로그래밍으로 계산합니다.
+# dp[i][j] :: 첫 번째 플레이어는 i번 카드까지, 두 번째 플레이어는 j번 카드까지 버렸을 때 나올 수 있는 최대 점수
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == -1:
+            continue
 
-for i in range(n+1) :
-    dp[i][0] = 0
-    dp[0][i] = 0
+        # 카드 대결 - 첫 번째 플레이어의 카드가 더 작은 경우
+        if a[i + 1] < b[j + 1]:
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
 
+        # 카드 대결 - 두 번째 플레이어의 카드가 더 작은 경우
+        if a[i + 1] > b[j + 1]:
+            dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + b[j + 1])
 
-maxi = 0
-for i in range(1,n+1) :
-    for j in range(1,n+1) :
-        count1,count2,count3,count4 = 0,0,0,0
+        # 카드 버리기
+        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j])
 
-        if b[i-1] < a[j] :
-            dp[i][j] = max(dp[i-1][j],dp[i][j])
-            count1 +=1
-        
-        if b[i]  > a[j-1] :
-            dp[i][j] = max(dp[i][j-1],dp[i][j])
-            count2 += 1
-        
-        if count1 == 0 and count2 == 0 :
-            if b[i-1] == a[j] :
-                dp[i][j] = dp[i-1][j]
-            
-            if b[i] == a[j-1] :
-                dp[i][j] = dp[i-1][j]
-                
-        
-        dp[i][j] = max(dp[i-1][j-1],dp[i][j])
-        
-        if a[j] > b[i] :
-            
-            dp[i][j] = dp[i][j] + b[i]
+# 결과를 계산하여 출력합니다.
+ans = 0
+for i in range(n + 1):
+    ans = max(ans, dp[i][n])
+    ans = max(ans, dp[n][i])
 
-            if i == n :  
-                maxi = max(maxi, dp[i][j])
-        
-        else :
-            if i==n or j  == n :
-                maxi = max(maxi, dp[i][j])
-
-
-
-print(maxi)
+print(ans)
