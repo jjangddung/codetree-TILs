3b872ae2d15a19a844496310bb94d674fd3034d3
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 21:06:12 +0900

    Title: 고대 보물 지도의 비밀, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -0,0 +1,34 @@
+import sys
+
+
+n,m = map(int, input().split())
+
+grid = [0] +  list(map(int, input().split()))
+
+# dp[i][j]  i개를 연속해서 골랐을 때, 음수의 개수가 j개일때의 최대값
+
+dp = [
+    [-1]*(3)
+    for _ in range(n+1)
+]
+
+
+dp[0][0] = 0
+
+maxi = -sys.maxsize
+for i in range(1,n+1) :
+    for j in range(3) :
+        for k in range(1,n+1-i): 
+            new_lst = grid[k:i+k]
+            count = 0
+            result = 0
+            for v in new_lst :
+                if v < 0 :
+                    count +=1
+                result += v
+            
+            if count == j :
+                dp[i][j] = max(dp[i][j], result)
+            maxi = max(dp[i][j],maxi)
+
+print(maxi)
