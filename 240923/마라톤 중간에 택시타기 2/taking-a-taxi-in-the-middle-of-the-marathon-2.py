8130b7706b4b81be0f27feba814f151ab55fdb5f
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 17:42:48 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240923/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(input())
+
+check_lst = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+prev_x, prev_y = check_lst[0][0], check_lst[0][1]
+mini = sys.maxsize
+
+for i in range(1,n-1) :
+    distance= 0
+
+    for j in range(n) :
+        if i == j :
+            continue
+        present_x,present_y = check_lst[j][0], check_lst[j][1]
+
+        distance += (abs(present_x-prev_x) + abs(present_y -prev_y))
+        prev_x,prev_y = present_x,present_y
+    
+    mini = min(distance,mini)
+        
+        
+print(mini)
