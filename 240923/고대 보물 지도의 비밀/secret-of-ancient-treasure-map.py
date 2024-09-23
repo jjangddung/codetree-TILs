26137e4b40a9974dc60261a6ccc701338af32129
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-23 22:17:28 +0900

    Title: 고대 보물 지도의 비밀, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 240923/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -5,29 +5,24 @@ n,m = map(int, input().split())
 
 grid = [0] +  list(map(int, input().split()))
 
-inf =-sys.maxsize
+inf =-10
 
-dp = [[[inf for _ in range(m+1)] for _ in range(n+1)] for _ in range(n+1)]
+dp = [
+    [inf]*(m+1)
+    for _ in range(n+1)
+]
 
-dp[0][0][0] = 0
 
-maxi = -sys.maxsize
+dp[0][0] = 0
 
 for i in range(1,n+1) :
-    for j in range(1,n+1) :
-        for k in range(m+1) :
-            if grid[j] < 0 :
-                if k-1 >= 0 :
-                    if dp[i-1][j-1][k-1] == inf :
-                        continue
-                    dp[i][j][k] = max(dp[i-1][j-1][k-1] + grid[j], dp[i][j][k])
-            
-            else :
-                if dp[i-1][j-1][k] == inf :
-                        continue
-                dp[i][j][k] = max(dp[i-1][j-1][k] + grid[j], dp[i][j][k])
-            
-            maxi = max(dp[i][j][k], maxi)
+    for j in range(m+1) :
+        if grid[i] < 0 :
+            if dp[i-1][j-1] != inf :
+                dp[i][j] = max(dp[i-1][j-1] + grid[i],dp[i][j])
+        
+        else :
+            if dp[i-1][j] != inf :
+                dp[i][j] = max(dp[i-1][j] + grid[i],dp[i][j])
 
-print(maxi)
-# print(dp)
+print(dp)
