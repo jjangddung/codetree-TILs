71b541b90ecdb73cd35492b38e9202119b36b8f1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-24 23:05:14 +0900

    Title: 숫자 합치기, Time: 5026ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 240924/숫자 합치기/merge-numbers.py
+++ 240924/숫자 합치기/merge-numbers.py
@@ -12,7 +12,7 @@ lst = list(map(int, input().split()))
 
 
 value = 0
-
+result = 0
 new_lst = copy.deepcopy(lst)
 
 # print(new_lst)
@@ -20,8 +20,11 @@ while len(new_lst) > 1 :
     new_lst = sorted(new_lst)
     # print(new_lst)
     value = new_lst[0] + new_lst[1]
+    result  += value
     new_lst = [value] +new_lst[2:]
     # print(new_lst)
 
 
-print(value + new_lst[0])
+
+if len(new_lst) == 1 :
+    print(result)
