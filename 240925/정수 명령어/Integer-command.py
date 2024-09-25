e6a26bb51ceadf23b299e6cf8ae7b3fcdb5d5b88
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-25 13:03:52 +0900

    Title: 정수 명령어, Time: 276ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240925/정수 명령어/Integer-command.py
@@ -0,0 +1,40 @@
+from sortedcontainers import SortedSet
+
+
+
+t = int(input())
+
+
+for i in range(t) :
+    k = int(input())
+    s = SortedSet()
+
+    for _ in range(k) :
+        
+        order = list(map(str, input().split()))
+        order, num = order[0] , int(order[1])
+
+        if order == 'I' :
+            s.add(num)
+        
+        else :
+            if len(s) != 0 :
+                if num == -1 :
+                    s.remove(s[0])
+
+                else :
+                    if len(s) != 1 :
+                        s.remove(s[-1])
+                    else :
+                        s.remove(s[0])
+        # print(s)
+
+    
+    if len(s) > 1 :
+        print(s[-1], s[0])
+    
+    elif len(s) == 1 :
+        print(s[0])
+    
+    else :
+        print("EMPTY")
