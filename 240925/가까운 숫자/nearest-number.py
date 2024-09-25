bc0960414ff1ae60752365550e6398e61253826f
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-25 14:27:28 +0900

    Title: 가까운 숫자, Time: 1505ms, Memory: 36MB, Status: Wrong Answer - CodeTree

--- 240925/가까운 숫자/nearest-number.py
+++ 240925/가까운 숫자/nearest-number.py
@@ -9,17 +9,30 @@ s.add(0)
 
 n = int(input())
 grid = list(map(int, input().split()))
+mini = sys.maxsize
+
+i = 0
 
 for g in grid :
     s.add(g)
-    mini = sys.maxsize
 
-    for num in s :
-        index = s.bisect_right(num)
+    if i == 0 :
+        mini = s[1] - s[0]
+    
+    else :
+        index1 = s.bisect_right(g)
+        index2 = index1 - 2
 
-        if index != len(s) :
-            result = abs(s[index]- num)
+        if index1 < len(s) and  0< index2 : 
+            result = min(abs(s[index1]- g), abs(s[index2] - g))
         
-        mini = min(result, mini)
+        elif 0 < index2 :
+            result = abs(s[index2]-g)
+        
+        elif index1 < len(s) :
+            result = abs(s[index1]-g)
+
+        mini = min(result,mini)
     
     print(mini)
+    i +=1
