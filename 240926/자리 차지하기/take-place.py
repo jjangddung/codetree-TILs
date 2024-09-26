f8e6373a47608967bf336c0231029fe4fc7b5b3d
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-26 13:36:54 +0900

    Title: 자리 차지하기, Time: 5082ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240926/자리 차지하기/take-place.py
+++ 240926/자리 차지하기/take-place.py
@@ -1,24 +1,15 @@
-from sortedcontainers import SortedSet
-
-
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 
 grid = list(map(int, input().split()))
 
+s = set()
 
+for t in grid:
+    # t에서부터 아래로 내려가면서 비어 있는 자리를 찾는다
+    while t > 0 and t in s:
+        t -= 1
 
-maxi = 0
-s =  SortedSet()
-# count = 0
-for i in range(n) :
-    t= grid[i]
-    while t >  0 and t  in s : 
-        t -=1
-
-    if t == 0 :
-        break
-    
-    else :
+    if t > 0:  # 0보다 크면, 해당 자리에 앉힐 수 있음
         s.add(t)
 
 print(len(s))
