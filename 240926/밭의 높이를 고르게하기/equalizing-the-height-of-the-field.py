865e4f3ee93cf568930d29f5399dbb49a3d70860
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-26 17:23:00 +0900

    Title: 밭의 높이를 고르게하기, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,25 @@
+import sys
+
+n,h,t = map(int, input().split())
+
+grid = list(map(int, input().split()))
+
+
+
+cost_lst = []
+
+
+for g in grid :
+    cost_lst.append(abs(g-h))
+
+# print(cost_lst)
+
+
+mini_cost = sys.maxsize
+
+for i in range(n-t) :
+    new_lst = cost_lst[i:i+t]
+    cost = sum(new_lst)
+    mini_cost = min(cost,mini_cost)
+
+print(mini_cost)
