0bbacd185544c9062534840a578d366647c99f8e
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-27 17:00:01 +0900

    Title: 자리 차지하기, Time: 396ms, Memory: 34MB, Status: Wrong Answer - CodeTree

--- 240927/자리 차지하기/take-place.py
+++ 240927/자리 차지하기/take-place.py
@@ -1,32 +1,20 @@
-from sortedcontainers import SortedSet
-
-
-n,m = map(int, input().split())
-
+n, m = map(int, input().split())
 grid = list(map(int, input().split()))
 
+parent = {}
 
-# 빈좌석중 최대값에 앉히기 ... ?
-
-maxi = 0
-s =  SortedSet() # 앉힌 자리로 했는데 ,,,, 빈 자리로 구성을 해볼까 >>>
-
-
-for i in range(1,m+1) :
-    s.add(i)
-
+def find(x):
+    if x not in parent:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
 
 count = 0
-
-for i in range(n) :
-    t= grid[i]
-    while t >  0 and t  in s : 
-        s.remove(t)
-        count +=1
-    if t == 0 :
-        break
-    
-    else  :
-        t-=1
+for t in grid:
+    p = find(t)
+    if p == 0:
+        continue
+    parent[p] = p - 1
+    count += 1
 
 print(count)
