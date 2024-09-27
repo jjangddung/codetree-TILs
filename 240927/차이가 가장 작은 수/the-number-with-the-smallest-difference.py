448b62e405de52a54eb2813eb836789fe8c8d817
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-27 17:18:38 +0900

    Title: 차이가 가장 작은 수, Time: 271ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240927/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
+++ 240927/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -6,29 +6,27 @@ input = sys.stdin.readline
 
 n,m = map(int, input().split())
 
-grid = [
-    int(input())
-    for _ in range(n)
-]
-
-
-
 s = SortedSet()
 
-
-for i in range(n) :
-    for j in range(i+1,n) :
-        result = abs(grid[i] -grid[j])
-
-        if result >= m :
-            if result == m :
-                s.add(result)
-                break
-            s.add(result)
+for _ in range(n) :
+    s.add(int(input()))
 
 
-if len(s) != 0 :
-    print(s[0])
 
-else :
-    print(-1)
+mini = sys.maxsize
+
+for v in s :
+    std = v + m
+
+    idx=  s.bisect_left(std) # v와 최소 m 이상만큼 차이나는 수 발견 ,,,,
+                             # 만약 idx = 0 or len(s)면 없는거임
+        
+    
+    if idx == 0  or idx == len(s) :
+        continue
+    
+    result = s[idx] -v 
+
+    mini = min(result, mini)
+
+print(mini)
