c95222afb58e1c9ad57a0a5b21b3a57ac6ae442a
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-30 18:22:52 +0900

    Title: 알파벳과 사칙연산, Time: 291ms, Memory: 26MB, Status: Passed - CodeTree

--- 240930/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 240930/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,79 +1,101 @@
 import sys
 
-input = sys.stdin.readline
+# input= sys.stdin.readline
 
-# 입력값 받기
-given = str(input()).strip()
-word_lst = []
+# 그냥 값을 미리 지정하기 
+given = str(input())
+word_lst =[]
 math_lst = []
 
-# 입력 문자열을 연산자와 숫자로 분리
-for s in given:
-    if s == '-' or s == '+' or s == '*':
-        math_lst.append(s)
-    else:
-        word_lst.append(s)
 
-word_set = list(set(word_lst))  # 중복 제거 후 리스트로 변환
+for s in range(len(given)) :
+    if given[s] == '-' or given[s] == '+' or given[s] == '*'  :
+        math_lst.append(given[s])
+    
+    else :
+        word_lst.append(given[s])
+
+
+# print(word_lst, math_lst)
+
+word_set = set(word_lst)
+
+word_set = list(word_set)
 
 ans_lst = []
 maxi = -sys.maxsize
+# prev_lst = []
 
-def backtracking(num, k):
+def backtracking(num,k) :
     global maxi
-    if k == 6:
+    if k == 6 :
+        # print(ans_lst)
         real_lst = []
         result_lst = []
 
-        for v in word_lst:
-            # 각 문자에 대해 ans_lst의 값을 실수 리스트로 변환
-            if v == 'a':
-                real_lst.append(str(ans_lst[0]))
-            elif v == 'b':
-                real_lst.append(str(ans_lst[1]))
-            elif v == 'c':
-                real_lst.append(str(ans_lst[2]))
-            elif v == 'd':
-                real_lst.append(str(ans_lst[3]))
-            elif v == 'e':
-                real_lst.append(str(ans_lst[4]))
-            elif v == 'f':
-                real_lst.append(str(ans_lst[5]))
 
+        for v in word_lst :
+            if v == 'a' :
+                real_lst.append(str(ans_lst[0]))
+            
+            elif v == 'b' :
+                real_lst.append(str(ans_lst[1]))
+            
+            elif v == 'c' :
+                real_lst.append(str(ans_lst[2]))
+            
+            elif v == 'd' :
+                real_lst.append(str(ans_lst[3]))
+            
+            elif v == 'e' :
+                real_lst.append(str(ans_lst[4]))
+            
+            elif v == 'f' :
+                real_lst.append(str(ans_lst[5]))
+        
         result_lst.append(real_lst[0])
 
-        if len(math_lst) == 0:
+        if len(math_lst) == 0 :
             value = int(real_lst[0])
             maxi = max(value, maxi)
-            return
+            return 
 
-        # 수식 리스트를 완성
-        for i in range(1, len(real_lst)):
-            if i - 1 < len(math_lst):  # 수식 범위가 math_lst를 넘지 않도록 범위 체크
-                result_lst.append(math_lst[i - 1])
+        for i in range(1, len(real_lst)) :
+            try :
+                result_lst.append(math_lst[i-1])
                 result_lst.append(real_lst[i])
+            except :
+                print(math_lst, real_lst)
+        
+        value = 0
+        for i in range(0,len(result_lst),2) :
+            if i  == 0 :
+                value += int(result_lst[i])
+                continue
+            
+            if result_lst[i-1] == '-' :
+                value -= int(result_lst[i])
+            
+            elif result_lst[i-1] == '+' :
+                value += int(result_lst[i])
+            
+            elif result_lst[i-1] == '*' :
+                value *= int(result_lst[i])
 
-        # 수식 계산
-        value = int(result_lst[0])
-        for i in range(1, len(result_lst), 2):
-            if result_lst[i] == '-':
-                value -= int(result_lst[i + 1])
-            elif result_lst[i] == '+':
-                value += int(result_lst[i + 1])
-            elif result_lst[i] == '*':
-                value *= int(result_lst[i + 1])
-
+        
         maxi = max(value, maxi)
-        return
 
-    # 가능한 값 탐색 (1부터 4까지)
-    for elem in range(1, 5):
+        return 
+
+    
+    for elem in range(1,5) :
         ans_lst.append(elem)
-        backtracking(elem, k + 1)
+        backtracking(elem,k+1)
         ans_lst.pop()
 
 if len(given) == 1:
     print(4)
-else:
-    backtracking(1, 0)
+
+else :
+    backtracking(1,0)
     print(maxi)
