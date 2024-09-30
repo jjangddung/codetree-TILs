f8f999dba1d9ddd47533ac636168e0706755c69e
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-09-30 16:09:30 +0900

    Title: 2명의 도둑, Time: 249ms, Memory: 27MB, Status: Passed - CodeTree

--- 240930/2명의 도둑/two-thieves.py
+++ 240930/2명의 도둑/two-thieves.py
@@ -70,50 +70,41 @@ ans_lst = []
 maximum = -sys.maxsize
 def backtracking(num,k) :
     global maximum
-
-    if num > n*n :
-        return 
-    
-    if num == n*n :
-        if k == 2 :
-            fisrt_v, second_v = ans_lst
-            # print(fisrt_v,second_v)
-            value = 0
-
-            one_r, one_c = fisrt_v//n, fisrt_v%n # 첫번째 리스트 시작점
-            second_c = one_c +m-1  # 첫번째 리스트 끝점
-            third_r, third_c = second_v//n, second_v% n # 두 번째 리스트 시작점
-            four_c =  third_c +m-1  # 두 번째 리스트 끝점
-
-            if second_c >= n or four_c >= n :
-                return 
-            
-            if overlap(one_r,third_r,second_c,third_c,four_c) :
-                return 
-
-            
-            lst1 = grid[one_r][one_c:second_c+1]
-            lst2 = grid[third_r][third_c:four_c +1]
+    if k == 2 :
+        fisrt_v, second_v = ans_lst
+        # print(fisrt_v,second_v)
+        value = 0
+        one_r, one_c = fisrt_v//n, fisrt_v%n # 첫번째 리스트 시작점
+        second_c = one_c +m-1  # 첫번째 리스트 끝점
+        third_r, third_c = second_v//n, second_v% n # 두 번째 리스트 시작점
+        four_c =  third_c +m-1  # 두 번째 리스트 끝점
+        if second_c >= n or four_c >= n :
+            return 
+        
+        if overlap(one_r,third_r,second_c,third_c,four_c) :
+            return 
+        
+        lst1 = grid[one_r][one_c:second_c+1]
+        lst2 = grid[third_r][third_c:four_c +1]
+        # print(lst1, lst2)
+        result = maxi(lst1) + maxi(lst2)
+#
+        # if fisrt_v == 0 and second_v == 13 :
             # print(lst1, lst2)
-
-            result = maxi(lst1) + maxi(lst2)
-# 
-            # if fisrt_v == 0 and second_v == 13 :
-                # print(lst1, lst2)
-                # print(maxi(lst1), maxi(lst2))
-            # print(result)
-# 
-            maximum = max(result, maximum)
-
-                
-            # print(ans_lst)
+            # print(maxi(lst1), maxi(lst2))
+        # print(result)
+#
+        maximum = max(result, maximum)
+            
+        # print(ans_lst)
         return
 
-    ans_lst.append(num)
-    backtracking(num+1,k+1)
-    ans_lst.pop()
-    backtracking(num+1,k)
+    for elem in range(num,n*n) :
+        ans_lst.append(elem)
+        backtracking(elem+1, k+1)
+        ans_lst.pop()
 
 backtracking(0,0)
+
 
 print(maximum)
