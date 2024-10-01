5d04ee7e2b4606a42183c3c48814aa8253848255
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-01 13:29:13 +0900

    Title: 앞에서부터 삭제하기 2, Time: 162ms, Memory: 31MB, Status: Passed - CodeTree

--- 241001/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241001/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,24 +1,39 @@
+import heapq
+
+
+
+import sys
+
 n = int(input())
-arr = [0] + list(map(int, input().split()))
 
-# 왼쪽에서부터 누적합을 계산 => O(N)
-prefix = [0 for _ in range(n+1)]
-for i in range(1, n+1):
-    prefix[i] = prefix[i-1] + arr[i]
+grid = list(map(int, input().split()))
 
-# 오른쪽에서부터 최소값을 기록 => O(N)
-temp = 10_001
-postfix = [0 for _ in range(n+1)]
-for i in range(n, -1, -1):
-    postfix[i] = min(temp, arr[i])
-    temp = postfix[i]
+grid = grid[::-1]
 
-total = sum(arr)
-ans = 0
-# 왼쪽부터 k개 원소를 삭제 => O(N)
-for k in range(1, n-1):
-    summation = total - prefix[k] - postfix[k+1]
-    avg = summation / (n-k-1)
-    ans = max(ans, avg)
+# pq = []
 
-print('{:.2f}'.format(ans))
+maxi = -sys.maxsize
+prev_sum = grid[0] + grid[1]
+prev_small = min(grid[0],grid[1])
+
+new_grid = [grid[0], grid[1]]
+pq = sorted(new_grid)
+
+# print(new_grid)
+prev_avg = (prev_sum - prev_small)/1
+# print(maxi)
+for k in range(2,n-1) :
+    heapq.heappush(pq, grid[k])
+    prev_sum += grid[k]
+    # print("pq: ", pq)
+    num = pq[0]
+    # print('num: ', num)
+    result = (prev_sum-num)/(k)
+    # print(result)
+    # heapq.heappush(pq, num)
+    maxi = max(result,maxi)
+    
+
+
+
+print(f'{maxi:.2f}')
