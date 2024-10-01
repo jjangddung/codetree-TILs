6425a929e22960d45695bcfcfa80bdbd7b4a119b
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-01 13:00:16 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5049ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 241001/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241001/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -8,16 +8,16 @@ n = int(input())
 
 grid = list(map(int, input().split()))
 
-
+grid = grid[::-1]
 
 pq = []
 
 maxi = -sys.maxsize
-for k in range(1,n-1) :
-    # new_grid = grid[k:]
+for k in range(2,n-1) :
+    new_grid = grid[:k]
     pq = []
-    for i in range(k,n) :
-        heapq.heappush(pq,grid[i])
+    for i in range (len(new_grid)) :
+        heapq.heappush(pq,new_grid[i])
     
     heapq.heappop(pq)
     result = sum(pq)/len(pq)
