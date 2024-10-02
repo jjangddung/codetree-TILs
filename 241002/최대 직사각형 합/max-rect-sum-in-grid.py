efdab29cbd01bd7c71475686c9421a44be1ad704
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-02 15:15:31 +0900

    Title: 최대 직사각형 합, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -0,0 +1,57 @@
+import sys
+
+
+n = int(input())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+for g in grid :
+    for v in range(n) :
+        g[v] += 1001
+# print(grid)
+
+
+prefix_sum = [
+    [0]*(n+1)
+    for _ in range(n+1)
+]
+
+prefix_sum[0][0] = 0
+
+
+for  i in range(1,n+1) :
+    prefix_sum[1][i] = prefix_sum[1][i-1] + grid[0][i-1]
+    prefix_sum[i][1] = prefix_sum[i-1][1] + grid[i-1][0]
+
+
+
+
+
+
+for i in range(1,n+1) :
+    for j in range(1,n+1) :
+        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] + grid[i-1][j-1] - prefix_sum[i-1][j-1]
+
+
+result = 0
+
+
+# for p in prefix_sum :
+    # print(*p)
+
+for x1 in range(1,n+1) :
+    for y1 in range(1,n+1) :
+        for x2 in range(1,n+1) :
+            for y2 in range(1,n+1) :
+                value = prefix_sum[x2][y2] -\
+                        prefix_sum[x2][y1-1] -\
+                        prefix_sum[x1-1][y2] +\
+                        prefix_sum[x1-1][y1-1]
+
+                value -= (1001)*(x2-x1+1)*(y2-y1+1) 
+                result = max(result,value)
+
+print(result)
