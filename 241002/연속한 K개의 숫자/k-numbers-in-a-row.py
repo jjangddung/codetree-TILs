b0150fcfc8f53cecbf039069d8b16d9867fc55f8
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-02 14:18:53 +0900

    Title: 연속한 K개의 숫자, Time: 161ms, Memory: 26MB, Status: Passed - CodeTree

--- 241002/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 241002/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,44 +1,31 @@
 import sys
 
+inf = sys.maxsize
 n,k,b = map(int, input().split())
+arr = [0]*(n+1)
+
+prefix_sum = [0]*(n+1)
+
+def get_sum(s,e) :
+    return prefix_sum[e] - prefix_sum[s-1]
 
 
-# k개가 연속하기 위해선????
-num_lst = []
-for i in range(1,n+1) :
-    num_lst.append(i)
+ans = inf
 
 
-zero_lst_index = []
 for _ in range(b) :
-    index = num_lst.index(int(input()))
-    # print(index)
-    num_lst[index] =  0
-    zero_lst_index.append(index)
-    # num_lst.pop(index)
-    # print(num_lst)
+    x = int(input())
+
+    arr[x] = 1 # 지워질 숫자에 1입력
 
 
-# print(num_lst)
-def backtracking(num,k) :
-    if k == p :
-        print(ans_lst)
-        return 
-    
-    
-    for i in range(num,n) :
-        ans_lst.append(num)
-        backtracking(num+1,k+1)
-        ans_lst.pop()
-
-result = sys.maxsize
+prefix_sum[0] = 0
 
 
-for i in range(n-k+1) :
-    count = 0
-    for t in range(i,i+k) :
-        if num_lst[t] == 0 :
-            count +=1
-    result = min(count, result)
+for i in range(1,n+1) :
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
 
-print(result)
+for i in range(1,n-k +2 ) :
+    ans = min(ans, get_sum(i,i+k-1))
+
+print(ans)
