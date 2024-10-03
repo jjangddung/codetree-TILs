18e9c3f1ed5e9fb383fcd23f3aebbbde1c12d9c3
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-03 14:57:13 +0900

    Title: 최대 직사각형 합, Time: 332ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 241003/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 241003/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -17,9 +17,9 @@ grid = [
     for _ in range(n)
 ]
 
-for g in grid :
-    for v in range(n) :
-        g[v] += 1001
+# for g in grid :
+    # for v in range(n) :
+        # g[v] += 1001
 # print(grid)
 
 
@@ -31,39 +31,92 @@ prefix_sum = [
 prefix_sum[0][0] = 0
 
 
-for  i in range(1,n+1) :
-    prefix_sum[1][i] = prefix_sum[1][i-1] + grid[0][i-1]
-    prefix_sum[i][1] = prefix_sum[i-1][1] + grid[i-1][0]
+# for  i in range(1,n+1) :
+    # prefix_sum[1][i] = prefix_sum[1][i-1] + grid[0][i-1]
+    # prefix_sum[i][1] = prefix_sum[i-1][1] + grid[i-1][0]
 
 
 
 
-
+# 행별로 부분합 구하기
 
 for i in range(1,n+1) :
     for j in range(1,n+1) :
-        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] + grid[i-1][j-1] - prefix_sum[i-1][j-1]
+        prefix_sum[i][j] = prefix_sum[i][j-1] + grid[i-1][j-1]
 
 
+# 행별로 부분합을 구했음
+
+# for p in prefix_sum :
+    # print(*p)
+# 
 result = -sys.maxsize
+
+# 카데인 알고리즘 적용
+inf = -sys.maxsize
+
+def kadane(temp) :
+    
+    max_value = inf
+
+    
+    length = len(temp)
+    value_lst = [inf] * (length+1)
+
+    for i in range(1,length) :
+        
+        value_lst[i] = max(value_lst[i-1]+ temp[i], temp[i])
+        max_value = max(value_lst[i], max_value)
+    
+    return max_value
+
+
+
+
+
+for left in range(1,n+1) :
+    temp = [0]* (n+1)
+    for right in range(left, n+1) :
+        for row in range(1,n+1) :
+            # temp[row] += (prefix_sum[row][right] - prefix_sum[row][left-1])
+            temp[row] = (prefix_sum[row][right] )
+
+        
+        # print(temp)
+        
+        calc = kadane(temp)
+        # print('calc: ', calc, left, right)
+
+        result = max(calc, result)
+
+
+print(result)
+        
+
+
+
+
+
+
+
 
 
 # for p in prefix_sum :
     # print(*p)
 
-for x1 in range(1,n+1) :
-    for y1 in range(1,n+1) :
-        for x2 in range(x1,n+1) :
-            for y2 in range(y1,n+1) :
-                value = prefix_sum[x2][y2] -\
-                        prefix_sum[x2][y1-1] -\
-                        prefix_sum[x1-1][y2] +\
-                        prefix_sum[x1-1][y1-1]
-
-
-                value -= (1001)*(x2-x1+1)*(y2-y1+1)
-                # if value == 900 :
-                    # print(f'x1: {x1} , y1 : {y1}, x2:{x2}, y2:{y2}, value: {value}') 
-                result = max(result,value)
-
-print(result)
+# for x1 in range(1,n+1) :
+    # for y1 in range(1,n+1) :
+        # for x2 in range(x1,n+1) :
+            # for y2 in range(y1,n+1) :
+                # value = prefix_sum[x2][y2] -\
+                        # prefix_sum[x2][y1-1] -\
+                        # prefix_sum[x1-1][y2] +\
+                        # prefix_sum[x1-1][y1-1]
+# 
+# 
+                # value -= (1001)*(x2-x1+1)*(y2-y1+1)
+                ##if value == 900 :
+                ##    print(f'x1: {x1} , y1 : {y1}, x2:{x2}, y2:{y2}, value: {value}') 
+                # result = max(result,value)
+# 
+# print(result)
