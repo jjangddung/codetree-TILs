3646fb9fbfcbb8d156dd4badaacbae25bc9ce449
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-04 12:32:29 +0900

    Title: 숫자 빠르게 찾기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241004/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,23 @@
+import sys
+n, m  =map(int, input().split())
+arr = list(map(int, input().split()))
+
+    # 오른쪽 구간으로 이동해야 합
+
+for i in range(m):
+    target  = int(input())
+    left, right = 0 , n-1
+    idx =-1
+
+    while left <= right :
+        mid = (left + right)//2
+        if arr[mid] == target :
+            idx = mid
+            break
+        
+        if arr[mid] > target :
+            right  = mid -1
+        
+        else :
+            left  = mid  +1
+    print(idx)
