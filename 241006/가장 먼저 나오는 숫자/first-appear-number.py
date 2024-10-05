8675e51c669a5010222f09d8af779a1af397aa82
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-06 03:02:10 +0900

    Title: 가장 먼저 나오는 숫자, Time: 234ms, Memory: 35MB, Status: Passed - CodeTree

--- 241006/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241006/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,10 +1,24 @@
-import sys
-n, m = map(int, input().split())
-grid = list(map(int, input().split()))
-num_lst = list(map(int, input().split()))
+import bisect
 
-for num in num_lst:
-    try :
-        print(grid.index(num)+1)
-    except :
-        print(-1)
+# 입력 받기
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = list(map(int, input().split()))
+
+# 결과 저장 리스트
+results = []
+
+# 각 질의에 대해 이진 탐색 수행
+for query in queries:
+    # 이진 탐색으로 최초 등장 위치 찾기
+    idx = bisect.bisect_left(arr, query)
+    
+    # 해당 위치가 배열 안에 있고, 그 위치의 값이 query와 같으면 출력
+    if idx < n and arr[idx] == query:
+        results.append(idx + 1)  # 1-based index이므로 +1
+    else:
+        results.append(-1)
+
+# 결과 출력
+for result in results:
+    print(result)
