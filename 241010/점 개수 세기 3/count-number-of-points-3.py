be61cad35bb6e4e2bd2244f9e91e74a718d2e57e
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-10-10 14:24:18 +0900

    Title: 점 개수 세기 3, Time: 792ms, Memory: 42MB, Status: Passed - CodeTree

--- 241010/점 개수 세기 3/count-number-of-points-3.py
+++ 241010/점 개수 세기 3/count-number-of-points-3.py
@@ -1,26 +1,32 @@
 from sortedcontainers import SortedSet
 
-
-n,q = map(int, input().split())
-
-
+n, q = map(int, input().split())
 points = list(map(int, input().split()))
-nums = SortedSet()
-for p in points :
-        nums.add(p)
-        
-for _ in range(q) :
-    mapper = dict()
-    cnt = 1
-    
-    s , e = map(int, input().split())
 
-    nums.add(s)
-    nums.add(e)
+# 중복 작업을 피하기 위해 한 번만 SortedSet을 생성
+nums = SortedSet(points)
 
-    for num in nums :
-        mapper[num] = cnt
+# 각 쿼리에서 추가되는 값을 미리 처리하기 위해 mapper도 한 번만 생성
+mapper = dict()
+cnt = 1
 
-        cnt +=1
-    
-    print(mapper[e]- mapper[s]+1)
+# nums에 미리 매핑을 만들어 놓음
+for num in nums:
+    mapper[num] = cnt
+    cnt += 1
+
+# 쿼리를 처리
+for _ in range(q):
+    s, e = map(int, input().split())
+
+    if s not in mapper:
+        nums.add(s)
+        mapper[s] = cnt
+        cnt += 1
+
+    if e not in mapper:
+        nums.add(e)
+        mapper[e] = cnt
+        cnt += 1
+
+    print(mapper[e] - mapper[s] + 1)
