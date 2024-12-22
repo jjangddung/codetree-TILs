997818fb3003ee9bce3747a07a8c2caef9397a17
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-12-22 16:27:15 +0900

    Title: A, B, C, D 찾기, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 241222/A, B, C, D 찾기/find-a-b-c-d.py
+++ 241222/A, B, C, D 찾기/find-a-b-c-d.py
@@ -7,18 +7,15 @@ num_list.sort()
 last_value = num_list[-1]
 
 a = num_list[0]
+b = num_list[1]
+c = num_list[2]
 
 def function(last_value,num_list) :
 
-    for b in range(a, last_value) :
-        for c in range(b, a+b+1) :
-            for d in range(c, last_value) :
-                new_list = []
-                new_list = [a,b,c,d,a+b,b+c,c+d,d+a,a+c,b+d,a+b+c,a+b+d,a+c+d,b+c+d,a+b+c+d]
-                new_list.sort()
-                if new_list == num_list :
-                    print(a,b,c,d)
-                    return
+    for d in range(c, last_value) :
+        if a+b+c+d == last_value :
+            print(a,b,c,d)
+            return 
     return
 
 function(last_value,num_list)
