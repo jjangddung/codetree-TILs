d17f45f0b41b84643301a61288d48a4c6e2e0cf1
jjangddung <123812155+jjangddung@users.noreply.github.com>
2024-12-22 16:24:52 +0900

    Title: A, B, C, D 찾기, Time: 5047ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

--- 241222/A, B, C, D 찾기/find-a-b-c-d.py
+++ 241222/A, B, C, D 찾기/find-a-b-c-d.py
@@ -6,19 +6,19 @@ num_list = list(map(int, input().split()))
 num_list.sort()
 last_value = num_list[-1]
 
+a = num_list[0]
 
 def function(last_value,num_list) :
 
-    for a in range(1,last_value) :
-        for b in range(a, last_value) :
-            for c in range(b, a+b+1) :
-                for d in range(c, last_value) :
-                    new_list = []
-                    new_list = [a,b,c,d,a+b,b+c,c+d,d+a,a+c,b+d,a+b+c,a+b+d,a+c+d,b+c+d,a+b+c+d]
-                    new_list.sort()
-                    if new_list == num_list :
-                        print(a,b,c,d)
-                        return
+    for b in range(a, last_value) :
+        for c in range(b, a+b+1) :
+            for d in range(c, last_value) :
+                new_list = []
+                new_list = [a,b,c,d,a+b,b+c,c+d,d+a,a+c,b+d,a+b+c,a+b+d,a+c+d,b+c+d,a+b+c+d]
+                new_list.sort()
+                if new_list == num_list :
+                    print(a,b,c,d)
+                    return
     return
 
 function(last_value,num_list)
